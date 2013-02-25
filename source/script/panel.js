f8f5aa697f44b24e41fd52773f060e714c064ea2
Sergey Kilipko <sergii.kylypko@globallogic.com>
2013-02-25 23:55:10 +0200

    create panel module and update default style

---
+++ source/script/panel.js
@@ -0,0 +1,100 @@
+eqnx.def('panel', function(panel, callback){
+
+	// use jquery, we can rid off this dependency
+	// if we will start using vanilla js functions
+	eqnx.use('jquery', 'ui', function($){
+
+		// timer needed for handling
+		// ui mistake - when user occasionally
+		// move mouse cursor out of panel
+		var timer;
+
+		// panel element
+		panel.create = function(){
+			// private variables
+			var frame, button;
+
+			// create element and add element id for proper styling
+			frame = $('<div>').attr('id', 'eqnx-panel');
+
+			// button helper
+			button = function(classname, event){
+				// create link element
+				$('<a>').
+					// add all classes
+					addClass('eqnx-panel-button ' + classname).
+
+					// handle click
+					click(function(){
+						eqnx.emit(event, frame);
+					}).
+
+					// append to frame
+					appendTo(frame);
+			}
+
+			// create panel buttons
+			button('eqnx-zoomin', 'zoom/increase');
+			button('eqnx-zoomout', 'zoom/decrease');
+			button('eqnx-tts', 'tts/toggle');
+
+			// add classes to zoom label
+			$('<span>').addClass('eqnx-panel-label eqnx-zoom').
+				html('100%').
+				appendTo(frame);
+
+			// return panel
+			return frame;
+		}
+
+		// show panel
+		panel.show = function(){
+			// clear timer if present
+			timer && clearTimeout(timer);
+
+			// already shown
+			if (panel.element) return;
+
+			// create new panel
+			panel.element = panel.create();
+
+			// hide panel when mouse leave panel
+			// use small delay just in case of ui mistake
+			panel.element.on('mouseleave', function(){
+				timer = setTimeout(panel.hide, 500);
+			});
+
+			// append to html
+			panel.element.hide().appendTo('html').fadeIn('fast', function(){
+				// notify system about shown panel
+				eqnx.emit('panel/show', panel.element);
+			});
+		}
+
+		// hide panel
+		panel.hide = function(){
+			// nothing to hide
+			if (!panel.element) return;
+
+			// hide panel
+			panel.element.fadeOut('fast', function(){
+				// notify about panel hiding
+				eqnx.emit('panel/hide', panel.element);
+
+				// remove element from dom
+				panel.element.remove();
+
+				// delete panel element
+				panel.element = undefined;
+			});
+		}
+
+		// setup trigger to show panel
+		eqnx.on('badge/hover', panel.show);
+
+		// panel is ready
+		callback();
+
+	});
+
+});
