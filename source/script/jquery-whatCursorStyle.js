635026bfabdc3eb4c8170180cf35243e7bcdf23b
Jonathan Barronville <mbarronville@gmail.com>
2013-03-07 14:51:58 -0500

    Converted our `.whatCursorStyle()` jQuery plugin to a function in our cursor implementation.

--- source/script/jquery-whatCursorStyle.js
+++
@@ -1,8 +0,0 @@
-eqnx.def('jquery.whatCursorStyle', function () {
-    eqnx.use('load', function (load) {
-        // TODO: Fix url.
-        load.script('//pastebin.com/raw.php?i=KJFxcmui', function () {
-            callback();
-        });
-    });
-});
