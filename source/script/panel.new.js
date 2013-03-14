5ef8359d904941d1ab911a0a5bea90f57389111e
Jonathan Barronville <mbarronville@gmail.com>
2013-03-14 19:44:53 -0400

    Still having trouble getting panel to work in our new module/event system. It would be great to get Sergey's assistance with this because I am a bit confused.

--- source/script/panel.new.js
+++ source/script/panel.new.js
@@ -1,127 +1,90 @@
 eqnx.def('panel', function (panel, callback) {
-    panel.init = function () {
-        this.isFloating = false;
-    };
+    eqnx.use('jquery', function (_jQuery) {
+        panel.kClosePanelTimeout = 500;
+        panel.kOffsetFromBadge   = 5;
+
+        panel.closePanel             = function () {
+            panel.panel.css('visibility', 'hidden');
+        };
+        panel.closePanelTimeoutFired = function () {
+            panel.timeoutId = 0;
+            panel.closePanel();
+        };
+        panel.findAndInitBadge       = function () {
+            if (!! panel.badge.length) {
+                return true;
+            }
+
+            panel.badge = _jQuery('#eq360-badge');
+
+            if (!! panel.badge.length) {
+                panel.panel     = _jQuery('#eq360-panel');
+                panel.ttsButton = _jQuery('#eq360-tts-button');
+
+                panel.setPositionAndSize(true);
+
+                panel.badge.css('visibility', 'visible');
+
+                _jQuery(window).on('resize', function () {
+                    panel.setPositionAndSize(true);
+                });
+
+                var mouseOverHandler      = function () {
+                    panel.onMouseOver();
+                };
+                var mouseOutHandler       = function () {
+                    panel.onMouseOut();
+                };
+                var clickTtsButtonHandler = function () {
+                    panel.onClickTtsButton();
+                };
+
+                panel.badge.on('mouseover', mouseOverHandler);
+                panel.panel.on('mouseover', mouseOverHandler);
+                panel.badge.on('mouseout', mouseOutHandler);
+                panel.panel.on('mouseout', mouseOutHandler);
+                panel.ttsButton.on('click', clickTtsButtonHandler);
+
+                panel.updateTtsButtonState();
+
+                return true;
+            }
+
+            return false;
+        };
+        panel.isExpanded             = function () {
+            return (panel.panel.css('visibility') === 'visible');
+        };
+        panel.setPositionAndSize     = function (reposition) {
+            if (typeof reposition === 'undefined') {
+                reposition = false;
+            }
+
+            var referenceWidth = window.innerWidth;
+
+            panel.badge.css('top', '15px');
+            panel.panel.css('top', ((15 + 35 + panel.kOffsetFromBadge).toString() + 'px'));
+            panel.badge.css('left', ((referenceWidth - (25 + 100)).toString() + 'px'));
+            panel.panel.css('left', ((referenceWidth - (25 + 205)).toString() + 'px'));
+        };
+        panel.showPanel              = function () {
+            panel.panel.css('visibility', 'visible');
+        };
+
+        panel.init                   = function () {
+            panel.isFloating = false;
+
+            if (! panel.findAndInitBadge()) {
+                _jQuery(document).ready(function () {
+                    _jQuery('html').append(
+                        '<div id="eq360-badge" class="eq360-ui" style="z-index: 2147483645; margin: 0; visibility: hidden; position: fixed; width: 100px; height: 30px;">' +
+                            '<img src="//ai2.s3.amazonaws.com/assets/newlogo.png" style="position: relative; width: 100px; height: 30px;"/>' +
+                        '</div>'
+                    );
+                });
+            }
+        };
+
+        callback();
+    });
 });
-
-var PanelView = (function () {
-    function PanelView() {
-        this.isFloating = false;
-        var panelView = this;
-        if(!this.findAndInitBadge()) {
-            Util.Ready.contentLoaded(function () {
-                var defaultBadgeLoader = new Util.Loader(panelView);
-                defaultBadgeLoader.appendHtml(DefaultBadgeTemplate.data, document.getElementsByTagName("html")[0]);
-            });
-        }
-    }
-    PanelView.kOffsetFromBadge = 5;
-    PanelView.kClosePanelTimeout = 500;
-    PanelView.prototype.observe = function (key) {
-        if(key === Util.ObserverKey.LoadComplete) {
-            this.findAndInitBadge();
-        } else if(key === Util.ObserverKey.TTSEnabled) {
-            this.updateTTSButtonState();
-        } else if(key === Util.ObserverKey.TTSEnabled) {
-            this.updateTTSButtonState();
-        }
-    };
-    PanelView.prototype.findAndInitBadge = function () {
-        var _this = this;
-        if(this.badge) {
-            return true;
-        }
-        this.badge = document.getElementById('eq360-badge');
-        if(this.badge) {
-            this.panel = document.getElementById('eq360-panel');
-            this.slider = new ZoomSlider();
-            this.ttsButton = document.getElementById('eq360-tts-button');
-            this.setPositionAndSize(true);
-            this.badge.style.visibility = 'visible';
-            var panelView = this;
-            window.addEventListener("resize", function () {
-                panelView.setPositionAndSize(true);
-            }, false);
-            var mouseOver = function () {
-                _this.onMouseOver();
-            };
-            this.badge.onmouseover = mouseOver;
-            this.panel.onmouseover = mouseOver;
-            var mouseOut = function () {
-                _this.onMouseOut();
-            };
-            this.badge.onmouseout = mouseOut;
-            this.panel.onmouseout = mouseOut;
-            var ttsButtonClick = function () {
-                _this.onTTSButtonClick();
-            };
-            this.ttsButton.onclick = ttsButtonClick;
-            eq360.addObserver(this);
-            eq360.isSpeechOn.addObserver(this);
-            this.updateTTSButtonState();
-            return true;
-        }
-        return false;
-    };
-    PanelView.prototype.setPositionAndSize = function (reposition) {
-        if (typeof reposition === "undefined") { reposition = false; }
-        var referenceWidth = window.innerWidth;
-        this.badge.style.top = "15px";
-        this.panel.style.top = (15 + 35 + PanelView.kOffsetFromBadge).toString() + "px";
-        this.badge.style.left = (referenceWidth - (25 + 100)).toString() + "px";
-        this.panel.style.left = (referenceWidth - (25 + 205)).toString() + "px";
-    };
-    PanelView.prototype.onMouseOver = function () {
-        eq360.initViews();
-        this.showPanel();
-        if(this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
-    };
-    PanelView.prototype.onMouseOut = function () {
-        var _this = this;
-        if(this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
-        this.timeoutId = setTimeout(function () {
-            _this.closePanelTimeoutFired();
-        }, PanelView.kClosePanelTimeout);
-    };
-    PanelView.prototype.onTTSButtonClick = function () {
-        eq360.isSpeechOn.save(!eq360.isSpeechOn.val());
-        this.updateTTSButtonState();
-    };
-    PanelView.prototype.updateTTSButtonState = function () {
-        var on = eq360.isSpeechOn.val();
-        this.ttsButton.setAttribute('aria-pressed', on ? 'true' : 'false');
-        this.ttsButton.style.backgroundPosition = (on ? "0% 0%" : "100% 0%");
-    };
-    PanelView.prototype.closePanelTimeoutFired = function () {
-        this.timeoutId = 0;
-        this.closePanel();
-    };
-    PanelView.prototype.closePanel = function () {
-        this.panel.style.visibility = 'hidden';
-    };
-    PanelView.prototype.showPanel = function () {
-        this.panel.style.visibility = 'visible';
-    };
-    PanelView.prototype.isExpanded = function () {
-        return this.panel.style.visibility === 'visible';
-    };
-    return PanelView;
-})();
-
-var PanelController = (function () {
-    function PanelController() {
-        this.loader = new Util.Loader(this);
-        this.loader.appendHtml(PanelTemplate.data, document.getElementsByTagName("html")[0]);
-    }
-    PanelController.prototype.observe = function (key) {
-        this.view = new PanelView();
-    };
-    PanelController.prototype.isExpanded = function () {
-        return this.view && this.view.isExpanded();
-    };
-    return PanelController;
-})();
