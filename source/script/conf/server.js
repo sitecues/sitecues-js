6e3900ed3c6e8f54223a610f1071663894c1f91d
Sergey Kilipko <sergii.kylypko@globallogic.com>
2013-03-20 15:41:41 +0200

    'conf/server' module for storing settings on the server

---
+++ source/script/conf/server.js
@@ -0,0 +1,26 @@
+eqnx.def('conf/server', function(server, callback){
+
+	// url of conf settings server
+	var url = '//up.ai2.at/preferences/' + location.host.replace(/^www\./, '') + '/em';
+
+	// depends on `conf` and `load` modules
+	eqnx.use('conf', 'load', function(conf, load){
+
+		// load data from server
+		load.script(url, function(){
+
+			// handle any `conf` setting change
+			conf.get('*', function(key, value){
+				// use image for cross-domain request to
+				// preferences server.
+				var img = document.createElement('img');
+
+				// set src to start request
+				img.setAttribute('src', url + '?' + key + '=' + value);
+			});
+
+		});
+
+	});
+
+});
