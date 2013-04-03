6707ef62a489da403a1738ed379026538f6d45e0
Jonathan Barronville <jbarronville@aisquared.com>
2013-04-03 12:59:50 -0400

    Fixed type checking bug.

--- source/script/invert.js
+++ source/script/invert.js
@@ -45,61 +45,52 @@ eqnx.def( 'invert', function ( invert, callback ) {
 
                 dom_highlight_box = $( event.highlight_box );
 
-                switch ( highlight_box_state ) {
-                    case (
-                        highlight_box_states.READY ||
-                        highlight_box_states.INFLATING ||
-                        highlight_box_states.CREATE
-                    ):
-                        switch ( current_state ) {
-                            case STATES.OFF:
-                                $( dom_highlight_box ).css( css_invert_on );
+                if (
+                    ( highlight_box_state === highlight_box_states.READY ) ||
+                    ( highlight_box_state === highlight_box_states.INFLATING ) ||
+                    ( highlight_box_state === highlight_box_states.CREATE )
+                ) {
+                    switch ( current_state ) {
+                        case STATES.OFF:
+                            $( dom_highlight_box ).css( css_invert_on );
 
-                                current_state = STATES.ON.HIGHLIGHT_BOX;
+                            current_state = STATES.ON.HIGHLIGHT_BOX;
 
-                                break;
-                            case STATES.ON.HIGHLIGHT_BOX:
-                                $( dom_highlight_box ).css( css_invert_off );
+                            break;
+                        case STATES.ON.HIGHLIGHT_BOX:
+                            $( dom_highlight_box ).css( css_invert_off );
 
-                                current_state = STATES.OFF;
+                            current_state = STATES.OFF;
 
-                                break;
-                            case STATES.ON.PAGE:
-                                $( dom_html ).css( css_invert_off );
-                                $( dom_highlight_box ).css( css_invert_on );
+                            break;
+                        case STATES.ON.PAGE:
+                            $( dom_html ).css( css_invert_off );
+                            $( dom_highlight_box ).css( css_invert_on );
 
-                                current_state = STATES.ON.HIGHLIGHT_BOX;
+                            current_state = STATES.ON.HIGHLIGHT_BOX;
 
-                                break;
-                        }
+                            break;
+                    }
+                } else if (
+                    ( highlight_box_state === highlight_box_states.ON ) ||
+                    ( highlight_box_state === highlight_box_states.CLOSED )
+                ) {
+                    switch ( current_state ) {
+                        case STATES.OFF:
+                            $( dom_html ).css( css_invert_on );
 
-                        break;
-                    case (
-                        highlight_box_states.ON ||
-                        highlight_box_states.CLOSED
-                    ):
-                        switch ( current_state ) {
-                            case STATES.OFF:
-                                $( dom_html ).css( css_invert_on );
+                            current_state = STATES.ON.PAGE;
 
-                                current_state = STATES.ON.PAGE;
+                            break;
+                        case STATES.ON.PAGE:
+                            $( dom_html ).css( css_invert_off );
 
-                                break;
-                            case STATES.ON.PAGE:
-                                $( dom_html ).css( css_invert_off );
+                            current_state = STATES.OFF;
 
-                                current_state = STATES.OFF;
-
-                                break;
-                        }
-
-                        break;
-                    case highlight_box_states.OFF:
-                        break;
-                    case highlight_box_states.DEFLATING:
-                        current_state = STATES.OFF;
-
-                        break;
+                            break;
+                    }
+                } else if ( highlight_box_state === highlight_box_states.DEFLATING ) {
+                    current_state = STATES.OFF;
                 }
 
                 if ( current_state === STATES.ON.HIGHLIGHT_BOX ) {
