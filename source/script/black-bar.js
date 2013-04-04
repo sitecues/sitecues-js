16fc1d36da39380abfd8eb292beec616bb821cf0
Jonathan Barronville <jbarronville@aisquared.com>
2013-04-04 16:32:11 -0400

    Renamed "black-bar" to a more generic "toolbar".

--- source/script/black-bar.js
+++ source/script/black-bar.js
@@ -1,6 +1,6 @@
-eqnx.def( 'black-bar', function ( black_bar, callback ) {
+eqnx.def( 'toolbar', function ( toolbar, callback ) {
     eqnx.use( 'jquery', 'ui', function ( $, ui ) {
-        black_bar.STATES = {
+        toolbar.STATES = {
             OFF: {
                 id:   0,
                 name: 'off'
@@ -12,57 +12,57 @@ eqnx.def( 'black-bar', function ( black_bar, callback ) {
         };
 
         var
-            STATES                                         = black_bar.STATES,
+            STATES                                         = toolbar.STATES,
             current_state                                  = STATES.OFF,
-            dom_black_bar_close_button_element_selector    = null,
-            dom_black_bar_close_button_element_id          = 'eqnx-black-bar-close-button',
-            dom_black_bar_element_selector                 = null,
-            dom_black_bar_element_id                       = 'eqnx-black-bar',
-            dom_black_bar_string                           = ('' +
-                '<div id="' + dom_black_bar_element_id + '">' +
-                    '<img id="' + dom_black_bar_close_button_element_id + '" src="//ai2.s3.amazonaws.com/assets/close.png" />' +
+            dom_toolbar_close_button_element_selector      = null,
+            dom_toolbar_close_button_element_id            = 'eqnx-toolbar-close-button',
+            dom_toolbar_element_selector                   = null,
+            dom_toolbar_element_id                         = 'eqnx-toolbar',
+            dom_toolbar_string                             = ('' +
+                '<div id="' + dom_toolbar_element_id + '">' +
+                    '<img id="' + dom_toolbar_close_button_element_id + '" src="//ai2.s3.amazonaws.com/assets/close.png" />' +
                 '</div>' +
             '')
         ;
 
-        dom_black_bar_element_selector              = ( '#' + dom_black_bar_element_id );
-        dom_black_bar_close_button_element_selector = ( '#' + dom_black_bar_close_button_element_id );
+        dom_toolbar_element_selector              = ( '#' + dom_toolbar_element_id );
+        dom_toolbar_close_button_element_selector = ( '#' + dom_toolbar_close_button_element_id );
 
-        black_bar.slideIn  = function () {
-            $( dom_black_bar_element_selector ).slideUp( 'slow' );
+        toolbar.slideIn  = function () {
+            $( dom_toolbar_element_selector ).slideUp( 'slow' );
 
-            eqnx.emit( 'black-bar/slide-in', $( dom_black_bar_element_selector ) );
+            eqnx.emit( 'toolbar/slide-in', $( dom_toolbar_element_selector ) );
 
             current_state = STATES.OFF;
 
-            eqnx.emit( ( 'black-bar/' + current_state.name ), $( dom_black_bar_element_selector ) );
+            eqnx.emit( ( 'toolbar/' + current_state.name ), $( dom_toolbar_element_selector ) );
         };
-        black_bar.slideOut = function () {
-            $( dom_black_bar_element_selector ).css( 'left', ( '-' + $( 'body' ).css( 'margin-left' ) ) );
-            $( dom_black_bar_element_selector ).css( 'top', ( '-' + $( 'body' ).css( 'margin-top' ) ) );
-            $( dom_black_bar_element_selector ).css( 'width', $( window ).width() );
-            $( dom_black_bar_element_selector ).slideDown( 'slow' );
+        toolbar.slideOut = function () {
+            $( dom_toolbar_element_selector ).css( 'left', ( '-' + $( 'body' ).css( 'margin-left' ) ) );
+            $( dom_toolbar_element_selector ).css( 'top', ( '-' + $( 'body' ).css( 'margin-top' ) ) );
+            $( dom_toolbar_element_selector ).css( 'width', $( window ).width() );
+            $( dom_toolbar_element_selector ).slideDown( 'slow' );
 
-            eqnx.emit( 'black-bar/slide-out', $( dom_black_bar_element_selector ) );
+            eqnx.emit( 'toolbar/slide-out', $( dom_toolbar_element_selector ) );
 
             current_state = STATES.ON;
 
-            eqnx.emit( ( 'black-bar/' + current_state.name ), $( dom_black_bar_element_selector ) );
+            eqnx.emit( ( 'toolbar/' + current_state.name ), $( dom_toolbar_element_selector ) );
         };
 
         $( document ).ready( function () {
-            $( 'body' ).prepend( dom_black_bar_string );
+            $( 'body' ).prepend( dom_toolbar_string );
 
-            eqnx.on( 'badge/hover', black_bar.slideOut );
+            eqnx.on( 'badge/hover', toolbar.slideOut );
 
-            $( dom_black_bar_close_button_element_selector ).on( 'click', black_bar.slideIn );
+            $( dom_toolbar_close_button_element_selector ).on( 'click', toolbar.slideIn );
         } );
 
-        eqnx.on( 'black-bar/on', function () {
-            console.log( 'Black bar state: [on].' );
+        eqnx.on( 'toolbar/on', function () {
+            console.log( 'Toolbar state: [on].' );
         } );
-        eqnx.on( 'black-bar/off', function () {
-            console.log( 'Black bar state: [off].' );
+        eqnx.on( 'toolbar/off', function () {
+            console.log( 'Toolbar state: [off].' );
         } );
 
         callback();
