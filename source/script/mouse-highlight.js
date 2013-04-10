ef019ffe1633d2fcd615d6310375b9a004a3c643
bwatson <bwatson@aisquared.com>
2013-04-10 10:56:41 -0400

    Revert HLB yellow bg fix as it was causing the highlight to disappear.

--- source/script/mouse-highlight.js
+++ source/script/mouse-highlight.js
@@ -1,134 +1,123 @@
 eqnx.def('mouse-highlight', function(mh, callback){
 
-	// minimum zoom level to enable highlight
-	// This is the default setting, the value used at runtime will be in conf.
-	mh.minzoom = 1.01;
+    // minimum zoom level to enable highlight
+    // This is the default setting, the value used at runtime will be in conf.
+    mh.minzoom = 1.01;
 
-	// how long mouse ptr needs to pause before we get a new highlight
-	mh.delay = 20;
-        
-        // if mouse highlight event handlers attached to window
-        mh.eventHandlersEnabled = false;
+    // how long mouse ptr needs to pause before we get a new highlight
+    mh.delay = 20;
 
-	// depends on jquery, conf and mouse-highlight/picker modules
-	eqnx.use('jquery', 'conf', 'mouse-highlight/picker', 'ui', function($, conf, picker){
+    // depends on jquery, conf and mouse-highlight/picker modules
+    eqnx.use('jquery', 'conf', 'mouse-highlight/picker', 'ui', function($, conf, picker){
 
-		// private variables
-		var timer;
+        // private variables
+        var timer;
 
-		conf.set('mouseHighlightMinZoom', mh.minzoom);
+        conf.set('mouseHighlightMinZoom', mh.minzoom);
 
-		// show mouse highlight
-		mh.show = function(element){
-			// get element to work with
-			element = element || mh.picked;
+        // show mouse highlight
+        mh.show = function(element){
+            // get element to work with
+            element = element || mh.picked;
 
-			// can't found any element to work with
-			if (!element) return;
+            // can't found any element to work with
+            if (!element) return;
 
-			// save styles
-			element.addClass('eqnx-highlight');
-		}
+            // save styles
+            element.addClass('eqnx-highlight');
+        }
 
-		// hide mouse highlight
-		mh.hide = function(element){
-			// get element to work with
-			element = element || mh.picked;
+        // hide mouse highlight
+        mh.hide = function(element){
+            // get element to work with
+            element = element || mh.picked;
 
-			// can't found any element to work with
-			if (!element) return;
+            // can't found any element to work with
+            if (!element) return;
 
-			// reset styles
-			element.removeClass('eqnx-highlight');
-		}
+            // reset styles
+            element.removeClass('eqnx-highlight');
+        }
 
-		mh.update = function(event){
-			// break if highlight is disabled
-			if (!mh.enabled) return;
+        mh.update = function(event){
+            // break if highlight is disabled
+            if (!mh.enabled) return;
 
-			// clear timeout if it was set
-			if (timer) clearTimeout(timer);
+            // clear timeout if it was set
+            if (timer) clearTimeout(timer);
 
-			// set new timeout
-			timer = setTimeout(function(){
-				if (mh.enabled && mh.eventHandlersEnabled && event.target !== mh.target){
-					// hide highlight for picked element
-					if (mh.picked) mh.hide(mh.picked);
+            // set new timeout
+            timer = setTimeout(function(){
+                if (event.target !== mh.target){
+                    // hide highlight for picked element
+                    if (mh.picked) mh.hide(mh.picked);
 
-					// save target element
-					mh.target = event.target;
+                    // save target element
+                    mh.target = event.target;
 
-					// save picked element
-					mh.picked = picker.find(event.target);
+                    // save picked element
+                    mh.picked = picker.find(event.target);
 
-					// show highlight for picked element
-					if (mh.picked && mh.picked.length)
-						mh.show(mh.picked);
-				} else {
-					// hide highlight for picked element
-					if (mh.picked && mh.picked.length)
-						mh.hide(mh.picked);
-					}
-			}, mh.delay);
-		}
+                    // show highlight for picked element
+                    if (mh.picked && mh.picked.length)
+                        mh.show(mh.picked);
+                }
+            }, mh.delay);
+        }
 
-		// refresh status of enhancement on page
-		mh.refresh = function(){
+        // refresh status of enhancement on page
+        mh.refresh = function(){
 
-			if (mh.enabled){
-				mh.eventHandlersEnabled = true;
-				// handle mouse move on body
-				$('body').on('mousemove', mh.update);
+            if (mh.enabled){
+                // handle mouse move on body
+                $('body').on('mousemove', mh.update);
 
-			} else {
-				mh.eventHandlersEnabled = false;
-				// remove mousemove listener from body
-				$('body').off('mousemove', mh.update);
+            } else {
+                // remove mousemove listener from body
+                $('body').off('mousemove', mh.update);
 
-				// hide highlight
-				mh.hide();
-			}
-		}
+                // hide highlight
+                mh.hide();
+            }
+        }
 
-		mh.updateZoom = function(zoom){
-			var was = mh.enabled;
-			mh.enabled = zoom >= conf.get('mouseHighlightMinZoom');
-			if (was !== mh.enabled) mh.refresh();
-		}
+        mh.updateZoom = function(zoom){
+            var was = mh.enabled;
+            mh.enabled = zoom >= conf.get('mouseHighlightMinZoom');
+            if (was !== mh.enabled) mh.refresh();
+        }
 
-		// hide mouse highlight once highlight box appears
-		eqnx.on('hlb/ready hlb/create', function(element){
-			// remove mousemove listener from body
-				mh.eventHandlersEnabled = false;
-			$('body').off('mousemove', mh.update);
-			mh.hide($(element));
-		});
+        // hide mouse highlight once highlight box appears
+        eqnx.on('hlb/ready hlb/create', function(element){
+            // remove mousemove listener from body
+            $('body').off('mousemove', mh.update);
+            mh.hide($(element));
+        });
 
-		// enable mouse highlight back once highlight box deflates
-		eqnx.on('hlb/closed', function(){
-			// handle mouse move on body
-				mh.eventHandlersEnabled = true;
-			$('body').on('mousemove', mh.update);
-		});
+        // enable mouse highlight back once highlight box deflates
+        eqnx.on('hlb/closed', function(){
+            // handle mouse move on body
+            $('body').on('mousemove', mh.update);
+        });
 
-		// handle zoom changes to toggle enhancement on/off
-		conf.get('zoom', mh.updateZoom);
+        // handle zoom changes to toggle enhancement on/off
+        conf.get('zoom', mh.updateZoom);
 
-		// Lower the threshold when speech is enabled.
-		eqnx.on('speech/enable', function(){
-			conf.set('mouseHighlightMinZoom', 1.00);
-			mh.updateZoom(conf.get('zoom'));
-		});
+        // Lower the threshold when speech is enabled.
+        eqnx.on('speech/enable', function(){
+            conf.set('mouseHighlightMinZoom', 1.00);
+            mh.updateZoom(conf.get('zoom'));
+        });
 
-		// Revert the threshold when speech is enabled.
-		eqnx.on('speech/disable', function(){
-			conf.set('mouseHighlightMinZoom', mh.minzoom);
-			mh.updateZoom(conf.get('zoom'));
-		});
+        // Revert the threshold when speech is enabled.
+        eqnx.on('speech/disable', function(){
+            conf.set('mouseHighlightMinZoom', mh.minzoom);
+            mh.updateZoom(conf.get('zoom'));
+        });
 
-		// done
-		callback();
+        // done
+        callback();
 
-	});
+    });
 
 });
