ba020f00d1f271dade09fc53f95b4c9e408a779d
akhabibullina <akhabibullina@aisquared.com>
2013-04-19 16:21:37 +0300

    EQ-349 EQ-350 EQ-351 EQ-395 Multiple fixes due to logic changes.

--- source/script/mouse-highlight.js
+++ source/script/mouse-highlight.js
@@ -4,53 +4,181 @@ eqnx.def('mouse-highlight', function(mh, callback){
     // This is the default setting, the value used at runtime will be in conf.
     mh.minzoom = 1.01;
 
-    // depends on jquery, conf and mouse-highlight/picker modules
-    eqnx.use('jquery', 'conf', 'mouse-highlight/picker', 'ui', function($, conf, picker){
+	// class of highlight
+    mh.kHighlightOverlayClass = 'eqnx-highlight-overlay';
+
+    // was highlight color avoided (in case of single media element just use outline)
+	mh.doPreventHighlightColor = false;
+
+    // was an overlay used to create the background color?
+	mh.doUseOverlayForBgColor = false;
+
+    // background color
+	mh.kBackgroundColor = 'rgba(250, 235, 200, 0.4)';
+
+    // saved CSS for highlighted element
+	mh.savedCss = [];
+
+    // adjacent paragraph rects are combined (for reading of articles / sections)
+	mh.kPixelsBeforeRectsCombined = 25;
+
+    // elements which need overlay for background color
+	mh.kVisualMediaElements = 'img,canvas,video,embed,object,iframe,frame';
+
+	// depends on jquery, conf, mouse-highlight/picker and positioning modules
+	eqnx.use('jquery', 'conf', 'mouse-highlight/picker', 'util/positioning', function($, conf, picker, positioning){
 
         conf.set('mouseHighlightMinZoom', mh.minzoom);
 
-        // show mouse highlight
-        mh.show = function(element){
-            // get element to work with
-            element = element || mh.picked;
+		mh.isBackgroundStyled = function(collection) {
+			var isBgStyled = false;
+			$(collection).each(function () {
+				// Is there any background on any element in collection or elements' ancestors or descendants
+				if ($(this).css('backgroundImage') !== 'none') {
+					isBgStyled = true;
+					return false;
+				}
+				var bgColor = $(this).css('backgroundColor');
+				if (bgColor !== 'rgba(0, 0, 0, 0)' && bgColor !== 'rgb(255, 255, 255)') {
+					isBgStyled = true;
+					return false;
+				}
+			});
+			return isBgStyled;
+		}
 
-            // can't found any element to work with
-            if (!element) return;
+		mh.hasFloatingSibling= function(collection) {
+			var allSiblings = $(collection).add($(collection).siblings());
+			var isNearFloat = false;
+			$(allSiblings).each(function () {
+				// Is there any background on any element in collection or elements' ancestors or descendants
+				if ($(this).css('float') !== 'none') {
+					isNearFloat = true;
+					return false;
+				}
+			});
+			return isNearFloat;
+		}
 
-            // save styles
-            element.addClass('eqnx-highlight');
-        }
+		// show mouse highlight
+		mh.show = function(collection){
+			// get element to work with
+			collection = collection || mh.picked;
 
-        // hide mouse highlight
-        mh.hide = function(element){
-            // get element to work with
-            element = element || mh.picked;
+			// can't found any element to work with
+			if (!collection) return;
 
-            // can't found any element to work with
-            if (!element) return;
+			var rects = positioning.getAllBoundingBoxes(collection, mh.kPixelsBeforeRectsCombined);
 
-            // reset styles
-            element.removeClass('eqnx-highlight');
-        }
+			// Multi-pronged approach.
+			// Determine which of the following approaches to use:
+			// 1. No background color, just outline
+			//    pros: does not interfere with media
+			//    cons: does not highlight text the way user expects
+			//    when-to-use: on lone media elements
+			// 2. overlay (absolutely positioned DOM element) for bg color (note: overlay still used for rounded outline)
+			//    pros: works over any collection of elements
+			//    cons: washes out foreground text (harder to read)
+			//    when-to-use: when CSS background changes would cause problems, e.g.
+			//                 highlighting multiple elements, existing backgrounds, floats, media
+			// 3. styling background (change the background-color + use overlay for rounded outline)
+			//    pros: foreground text does not get washed out
+			//    cons: creates discontiguous areas so does not work when > 1 elements to be highlighted,
+			//          does not work well when there is a background or background image
+			//    when-to-use: whenever we can without breaking -- this is the preferred approach
+			mh.doPreventHighlightColor = false;
+			mh.doUseOverlayForBgColor = false;
+			if (collection.length === 1 && ($(collection).is(mh.kVisualMediaElements) ||
+				mh.isBackgroundStyled(collection))) {
+				// approach #1 -- overlay for outline only
+				mh.doPreventHighlightColor = true;
+			}
+			else {
+				if ($(collection).length > 1 ||
+					$(collection).is(mh.kVisualMediaElements) ||
+					$(collection).find(mh.kVisualMediaElements).length ||
+					mh.isBackgroundStyled($(collection).add($(collection).parentsUntil(document.body)).add($(collection).find('> *, > * > *'))) ||
+					mh.hasFloatingSibling(collection)) {
+					// approach #2 -- overlay for background color and rounded border
+					mh.doUseOverlayForBgColor = true;
+				}
+				else {
+					// approach #3 -- change css background of element + use overlay for rounded border
+					mh.doUseOverlayForBgColor = false;
+				}
+			}
 
-        mh.update = function(event){
+			// Position each focus rect absolutely over the item which is focused
+			for (var count = 0; count < rects.length; count ++) {
+				var rect = rects[count];
+				$('<div>')
+					.attr('class', mh.kHighlightOverlayClass)
+					.css({
+						top: rect.top - 3,
+						left: rect.left - 3,
+						width: rect.width + 4,
+						height: rect.height + 4,
+						display: 'block'
+					})
+					.appendTo(document.body);
+			}
+
+			// Add highlight color if necessary
+			if (!mh.doPreventHighlightColor) {
+				if (mh.doUseOverlayForBgColor) {
+					$('.' + mh.kHighlightOverlayClass).css('backgroundColor', mh.kBackgroundColor);
+				}
+				else {
+					// We only do this for single elements -- multiple items always get the overlay
+					var element = collection.get(0);
+					mh.savedCss = {
+						backgroundColor: element.style.backgroundColor,
+						outlineWidth: element.style.outlineWidth,
+						outlineStyle: element.style.outlineStyle,
+						outlineColor: element.style.outlineColor,
+						outlineOffset: element.style.outlineOffset
+					};
+					$(element).css( {
+						backgroundColor: mh.kBackgroundColor,
+						outlineWidth: '4px',
+						outlineStyle: 'solid',
+						outlineColor: 'rgba(250, 235, 200, .2)',
+						outlineOffset: '-3px'
+					});
+				}
+			}
+		}
+
+		// hide mouse highlight
+		mh.hide = function(collection){
+			if (collection && !mh.doPreventHighlightColor && !mh.doUseOverlayForBgColor && mh.savedCss) {
+				$(collection).css(mh.savedCss);
+				mh.savedCss = null;
+			}
+			$('.' + mh.kHighlightOverlayClass).remove();
+			collection = null;
+		}
+
+		mh.update = function(event){
             // break if highlight is disabled
             if (!mh.enabled) return;
 
-                if (event.target !== mh.target){
-                    // hide highlight for picked element
-                    if (mh.picked) mh.hide(mh.picked);
+           if (event.target !== mh.target){
+                // hide highlight for picked element
 
-                    // save target element
-                    mh.target = event.target;
+                if (mh.picked) mh.hide(mh.picked);
 
-                    // save picked element
-                    mh.picked = picker.find(event.target);
+                // save target element
+                mh.target = event.target;
 
-                    // show highlight for picked element
-                    if (mh.picked && mh.picked.length)
-                        mh.show(mh.picked);
+                // save picked element
+                mh.picked = picker.find(event.target);
+
+                // show highlight for picked element
+                if (mh.picked && mh.picked.length) {
+                    mh.show(mh.picked);
                 }
+            }
 
         }
 
@@ -60,7 +188,6 @@ eqnx.def('mouse-highlight', function(mh, callback){
             if (mh.enabled){
                 // handle mouse move on body
                 $('body').on('mousemove', mh.update);
-
             } else {
                 // remove mousemove listener from body
                 $('body').off('mousemove', mh.update);
