82356e25dc81788086476da343f7c86b60146c34
akhabibullina <akhabibullina@aisquared.com>
2013-05-24 16:10:28 +0300

    EQ-113 Set default custom cursor image for disabled elements.

--- source/js/cursor/custom.js
+++ source/js/cursor/custom.js
@@ -19,7 +19,8 @@ sitecues.def('cursor/custom', function (cursor, callback) {
 	cursor.url = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAaCAYAAAC3g3x9AAAB+klEQVRIS+2UPUvDQBzGe219ado6qHR0KP0QDoJSQRFHJ8HRtXR3cBI/QLfiKgo6OQp2dhG/QSmFKmqwviSm0TatzwOXkkvSQKE4efBwudzd7//KidiEh5gwL/Z3wMFgsCmEuB43glAPAbsBaB1qAlqQ0AHmPsR55AgDin6/bwM0zVu6rpdyudyphH1j/omCBoCO45zF4/Fd14VOp3OvadoO1j3oE/qCnFEuBoAIt4nDS+4FGDCTyeQK1vTuHXqT36FMBdjr9fYTicSJ/2S9Xr8qFApH+N9mFiBzVNgKELm7Re6WCQS8Dc/m+W2aZiubze5J754kmCkIDC9QKYb/ZKVSOSyXyzX8f4QItSOB/mL4Dzcajbt8Pn8ggS3MBsQ2UsbQQxTjAztzoZnGz263a1Wr1eNSqcRmf4BeIbaQCvQ0sbohRBG5u0in04veDdu2n1Op1JoMm8VhCw2bnXmroRDFgCUhNgzDOM9kMgvePcuydBjZll6yjZjLYegiwsNVXL6ENzkvEI3+gkbfkh4GgdiIQymZPw1zEkpICEOZgmblzJwzRIbKHEYCMzgwA7mFYp91PQYJpTEaYTFYRFZaDVle4GF6yQfBD+SaXtKYC6ShjlQAyAsMkWL47nCfK6753xXXLAJDZxTKk/Z3L7bH07E+/z0cK12hh38BlI3AG1Ei3IIAAAAASUVORK5CYII=";
 
 	// constants
-	cursor.kCursorHideRuleId = 'sitecues-cursor-hide-rule';
+	cursor.kCursorStyleRuleId = 'sitecues-cursor-style-rule';
+	cursor.kCursorStyleDisabledRuleId  = 'sitecues-cursor-disabled-rule';
 	cursor.kCursorId = 'sitecues-cursor';
 	cursor.kMinCursorZoom = 1.1;
 
@@ -54,7 +55,8 @@ sitecues.def('cursor/custom', function (cursor, callback) {
 		 *  Show custom cursor in the viewport.
 		 */
 		cursor.show = function() {
-			cursor.styleRuleParent.append('<style id="' + cursor.kCursorHideRuleId + '">* { cursor: url("' + cursor.url + '"), ' + cursor.type +'}');
+			cursor.styleRuleParent.append('<style id="' + cursor.kCursorStyleRuleId + '">* { cursor: url("' + cursor.url + '"), ' + cursor.type +'}');
+			cursor.styleRuleParent.append('<style id="' + cursor.kCursorStyleDisabledRuleId + '">*:disabled { cursor: url("' + view.getImageOfType('default') + '"), default}');
 			$(window).on('mousemove click', mouseMoveHandler);
 			sitecues.emit('cursor/show');
 		};
@@ -63,8 +65,10 @@ sitecues.def('cursor/custom', function (cursor, callback) {
 		 *  Update cursor properties, such as dimensions or color.
 		 */
 		cursor.update = function() {
+			$('#' + cursor.kCursorStyleDisabledRuleId).remove();
 			// Target is not changed, so update the same element's cursor style.
 			$(cursor.prevTarget).css('cursor', 'url("' + cursor.url + '"), ' + cursor.type);
+			cursor.styleRuleParent.append('<style id="' + cursor.kCursorStyleDisabledRuleId + '">*:disabled { cursor: url("' + view.getImageOfType('default') + '"), default}');
 			sitecues.emit('cursor/update');
 		};
 
@@ -73,6 +77,7 @@ sitecues.def('cursor/custom', function (cursor, callback) {
 		 */
 		cursor.hide = function() {
 			// Reset the CSS cursor style.
+			$('#' + cursor.kCursorStyleDisabledRuleId).remove();
 			$(cursor.prevTarget).css('cursor', cursor.prevType);
 			$(window).off('mousemove click', mouseMoveHandler);
 			sitecues.emit('cursor/hide');
@@ -85,7 +90,7 @@ sitecues.def('cursor/custom', function (cursor, callback) {
 		 * @param target
 		 */
 		function changeCursorDisplay(target) {
-			$('#' + cursor.kCursorHideRuleId).remove();
+			$('#' + cursor.kCursorStyleRuleId).remove();
 			
 			// If target is changed then update CSS cursor property for it.
 			if (cursor.isEnabled && !$(target).is(cursor.prevTarget)) {
