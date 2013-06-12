aa7e1f8bac704243a742ef7ba6d611342d339029
Alistair MacDonald <amacdonald@aisquared.com>
2013-06-12 14:01:04 -0400

    EQ-649: Adding a function log.toggleConsoleLog() which routes logging to the JavaScript console in prod.

--- source/js/logging/log4javascript_production.js
+++ source/js/logging/log4javascript_production.js
@@ -1,14 +1,35 @@
 var log4javascript = {
 
+  consoleLog: false,
+
   getLogger: function(){
     
     var log = function(){};
-    log.prototype.trace = function(){};
-    log.prototype.debug = function(){};
-    log.prototype.info = function(){};
-    log.prototype.warn = function(){};
-    log.prototype.error = function(){};
-    log.prototype.fatal = function(){};
+    
+    // Redirect to JavaScript Console
+    log.prototype.cl = function(r){
+      if (this.parent.consoleLog) {
+        console.log(r);
+      }
+    };
+
+    log.prototype.parent = this;
+    log.prototype.toggleConsoleLog = function(){
+      if (this.parent.consoleLog) {
+        this.parent.consoleLog = false;
+        return "Off"
+      } else {
+        this.parent.consoleLog = true;
+        return "On"
+      }
+    };
+
+    log.prototype.trace = function(r){ this.cl(r); };
+    log.prototype.debug = function(r){ this.cl(r); };
+    log.prototype.info = function(r){ this.cl(r); };
+    log.prototype.warn = function(r){ this.cl(r); };
+    log.prototype.error = function(r){ this.cl(r); };
+    log.prototype.fatal = function(r){ this.cl(r); };
     log.prototype.addAppender = function(){};
     log.prototype.getLogger = function(){};
 
