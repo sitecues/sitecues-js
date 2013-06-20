9533f84de45d19831e4797f095f7a8818d76b8cc
Eric Savage <esavage@aisquared.com>
2013-06-20 15:39:56 -0400

    EQ-667 Fix link style, auto-adjust font-size

--- source/js/toolbar/dropdown.js
+++ source/js/toolbar/dropdown.js
@@ -1,24 +1,35 @@
 sitecues.def('toolbar/dropdown', function(dropdown, callback, log){
     sitecues.use( 'jquery', 'toolbar/bootstrap-dropdown', function ($, bootstrapDropdown) {
       /**
-       * We're not going to do this automatically as we need to make sure the toolbar is on the page to set up the
-       * listeners properly. Otherwise we'd have to set the .on() methods to document scope which would be a
+       * We're not going to do this automatically as we need to make sure the
+       * toolbar is on the page to set up the listeners properly. Otherwise
+       * we'd have to set the .on() methods to document scope which would be a
        * performance hit.
-       *
+       * 
        * @return void
        */
-      dropdown.build = function(parent) {
+      dropdown.build = function(toolbar) {
 
-        var dropdownWrap = $('<div class="dropdown-wrap"></div>').prependTo(parent);
-        var dropdownLink = $('<a class="dropdown-toggle" data-toggle="dropdown" href="#"><span>sitecues</span></a>').appendTo(dropdownWrap);
-        var dropdownMenu = $('<ul class="dropdown-menu" role="menu"></ul>').appendTo(dropdownWrap);
+        dropdown.wrap = $('<div class="dropdown-wrap"></div>').prependTo(toolbar);
+        var dropdownLink = $('<a class="dropdown-toggle" data-toggle="dropdown" href="#"><span>sitecues</span></a>').appendTo(dropdown.wrap);
+        var dropdownMenu = $('<ul class="dropdown-menu" role="menu"></ul>').appendTo(dropdown.wrap);
         $('<li><a href="#">Get Help</a></li>').appendTo(dropdownMenu);
         $('<li><a href="#">Provide feedback</a></li>').appendTo(dropdownMenu);
         $('<li><a rel="sitecues-event" data-sitecues-event="inverse/toggle">Change page colors</a></li>').appendTo(dropdownMenu);
         $('<li><a rel="sitecues-event" data-sitecues-event="toolbar/disable">Turn off</a></li>').appendTo(dropdownMenu);
-
+        dropdown.updateFontSize(toolbar);
         return;
       }
+
+      dropdown.updateFontSize = function(toolbar) {
+        var fontSize = (toolbar.height() * 0.4);
+        if(fontSize < 15) {
+          fontSize = 15;
+        }
+        dropdown.wrap.css('font-size', fontSize + 'px')
+      }
+
+      sitecues.on("toolbar/resized", dropdown.updateFontSize);
 
       callback();
     });
