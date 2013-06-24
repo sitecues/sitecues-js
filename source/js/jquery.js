ac2b886fc2b507a5a8b15fbc2b793b43c7bf9773
Jonathan Barronville <jbarronville@aisquared.com>
2013-06-24 10:17:40 -0400

    Cleaned up a few things. Also pushing in fix for #EQ-659.

--- source/js/jquery.js
+++ source/js/jquery.js
@@ -1,15 +1,7 @@
 sitecues.def('jquery', function (module, callback, log) {
-
-	// initiate loading from google cdn. after loading
-	// switch if to no-confict mode in case of host
-	// page already using $ for some purposes
-	sitecues.use('load', function(load){
-		load.script(
-			'//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-			function(){
-				callback(jQuery.noConflict(true));
-			}
-		);
+	sitecues.use('load', function (load) {
+		load.script('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function () {
+			callback(jQuery.noConflict(true));
+		});
 	});
-
 });
