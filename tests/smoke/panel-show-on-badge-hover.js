7e9959b3263d93812295d59911958ce5e8dcad2d
Alistair MacDonald <amacdonald@aisquared.com>
2013-06-27 17:56:43 -0400

    Working on tests.

--- tests/smoke/panel-show-on-badge-hover.js
+++ tests/smoke/panel-show-on-badge-hover.js
@@ -4,6 +4,33 @@ var
   expect  = chai.expect
   ;
 
+// var startID = (+ new Date());
+
+var onError = function(e){
+  this.or = function(callback) {
+    callback();
+  };
+
+  if (err) {
+    console.log( e.message );
+  }else{
+    return this;
+    
+  }
+};
+
+
+
+var log = function(e){
+  if(typeof e == "object"){
+   console.log(JSON.stringify(e))
+  } else {
+    console.log(e);
+  }
+  return;
+};
+
+
 describe("sitecues", function () {
 
   describe("navigator", function () {
@@ -11,6 +38,7 @@ describe("sitecues", function () {
     swdda.describeForEachBrowser('panel-show on badge hover', function(s) {
 
       it("should navigate to the simple ebank page", function (done) {
+        log("loaded page");
         s.browser.get(swdda.testUrl('/site/eBank/index.html'), function() {
           s.browser.title(function(err, title) {
             expect(title).to.be.a("string");
@@ -21,13 +49,38 @@ describe("sitecues", function () {
       });
 
 
-      
-      it("should see the badge on the page", function (done) {
-        s.browser.waitForVisibleById('sitecues-badge', 3000, function(err) {
-          expect(err).to.be.null;
+      it("testing div#main", function (done) {
+        s.browser.waitForVisibleById('main', 3000, function(err) {
+          onError(err).or(function(){
+            console.log("Aaaaaaa!");
+          });
           done();
         });
       });
+
+      // it("testing div#impossible", function (done) {
+      //   s.browser.waitForVisibleById('impossible10861461304', 3000, function(err) {
+      //     log(err instanceof Error);
+      //     if(err==null){
+      //       log("______________________________________");
+      //       log("found:div#impossible");
+      //     }else{
+      //       log("impossible:ERROR!");
+      //       //log(err.stack);
+      //       log(err.message);
+      //     }
+      //     done();
+      //   });
+      // });
+
+      // it("should see the badge on the page", function (done) {
+      //   s.browser.waitForVisibleById('sitecues-badge', 3000, function(err) {
+      //     log("see sitecues badge");
+      //     log(err);
+      //     log(arguments);
+      //     done();
+      //   });
+      // });
 
       // var sitecuesBagdeElem;
 
