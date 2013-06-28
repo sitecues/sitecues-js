4aec58310e61abe77b78efcd828286e6ce11ae8b
Jonathan Barronville <jbarronville@aisquared.com>
2013-06-28 16:59:22 -0400

    Pushing in a basic toolbar smoke test. #EQ-690.

---
+++ tests/smoke/toolbar.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// Imports.
+var
+  chai  = require("chai"),
+  swdda = require("swdda"),
+  wd    = require("wd")
+;
+
+// Setup.
+var
+  expect  = chai.expect,
+  globals = {}
+;
+
+describe("sitecues", function () {
+  describe("navigator", function () {
+    before(function (done) {
+      globals.url = swdda.testUrl("/site/simple.html");
+    });
+
+    swdda.describeForEachBrowser("toolbar", function (session) {
+      it("should navigate to <sitecues_test_pages>/simple.html", function (done) {
+        session.browser.get(globals.url, function (error) {
+          expect(error).to.not.be.an.instanceof(Error);
+
+          session.browser.title(function (error, title) {
+            expect(error).to.not.be.an.instanceof(Error);
+            expect(title).is.equal("Young Frankenstein");
+          });
+        });
+
+        done();
+      });
+
+      it("should see toolbar on the page on F8", function (done) {
+        session.browser.keys(wd.SPECIAL_KEYS["F8"], function (error) {
+          expect(error).to.not.be.an.instanceof(Error);
+
+          session.browser.waitForElementByClassName(
+            "sitecues-toolbar",
+            500,
+            function (error) {
+              expect(error).to.not.be.an.instanceof(Error);
+            }
+          );
+        });
+
+        done();
+      });
+    });
+  });
+});
