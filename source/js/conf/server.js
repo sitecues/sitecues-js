f012b5122c212986395add07f914fed913e4b343
bwatson <bwatson@aisquared.com>
2013-07-07 17:09:04 -0400

    EQ-465, EQ-676: Converted new AJAX calls to JSONP, as there was a problem with the user ID cookie being communicated between the browser and the server.

--- source/js/conf/server.js
+++ source/js/conf/server.js
@@ -1,10 +1,13 @@
 // module for storing settings on the server
 sitecues.def('conf/server', function (server, callback, log) {
 
-	// URLs for loading/saving data
+  // Used for JSONP callbacks.
+  var extConf = sitecues.conf = sitecues.conf || {};
+
+  // URLs for loading/saving data
   var domain = location.hostname.replace(/^www\./, '');
-	var saveUrl = '//' + sitecues.getCoreConfig().hosts.up + '/save/' + domain;
-  var loadUrl = '//' + sitecues.getCoreConfig().hosts.up + '/load/' + domain;
+	var saveUrl = '//' + sitecues.getCoreConfig().hosts.up + '/save/' + domain + '?callback=?';
+  var loadUrl = '//' + sitecues.getCoreConfig().hosts.up + '/load/' + domain + '?callback=?';
 
   // UGH!!! The preferences server has a race condition that can clobber data! And ...
   // Redis has no transactions (at least it is web-scale). So, THIS code must ensure
@@ -14,6 +17,13 @@ sitecues.def('conf/server', function (server, callback, log) {
   var SAVING_DATA = false;
 
 	sitecues.use('conf', 'load', 'jquery', function(conf, load, jquery){
+
+    // JSONP callback called when a save call returns.
+    var saveTimeoutId;
+    extConf.saveCallback = function() {
+      saveTimeoutId && clearTimeout(saveTimeoutId);
+      SAVING_DATA = false;
+    };
 
     // Saves a key/value pair.
     var saveData = function(key, value) {
@@ -25,16 +35,50 @@ sitecues.def('conf/server', function (server, callback, log) {
       } else {
         // Save the data.
         SAVING_DATA = true;
+
+        // Set up the data.
         var data = {};
         data[key] = value;
+
+        // Set a save set timeout.
+        saveTimeoutId = setTimeout(function() {
+          extConf.saveCallback()
+        }, 500);
+
         jquery.ajax({
-          type: 'POST',
+          type: 'GET',
           url: saveUrl,
           data: data,
-          complete: function() {
-            SAVING_DATA = false;
+          //async: false,
+          contentType: "application/json",
+          dataType: 'jsonp',
+          success: function(data) {
+            extConf.saveCallback();
+          },
+          error: function(e) {
+            log.info("Unable to persist server config (" + key + "=" + value + "): " + e.message);
+            extConf.saveCallback();
           }
         })
+      }
+    };
+
+    // JSONP callback called when a load call returns.
+    var loadTimeoutID;
+    var initialized = false;
+    extConf.loadCallback = function(data) {
+      loadTimeoutID && clearTimeout(loadTimeoutID);
+      // Set the obtained config data (if any).
+      data && conf.data(data);
+
+      if (!initialized) {
+        initialized = true;
+        // Update the preferences server on every 'set'.
+        conf.get('*', function(key, value) {
+          saveData(key, value);
+        });
+        // This module has completed it's loading.
+        callback();
       }
     };
 
@@ -42,16 +86,15 @@ sitecues.def('conf/server', function (server, callback, log) {
     jquery.ajax({
       type: 'GET',
       url: loadUrl,
+      //async: false,
+      contentType: "application/json",
+      dataType: 'jsonp',
       success: function(data) {
-        // Set the obtained config data.
-        conf.data(data);
-        // Update the preferences server on every 'set'.
-        conf.get('*', function(key, value) {
-          saveData(key, value);
-        });
+        extConf.loadCallback(data);
       },
-      complete: function() {
-        callback();
+      error: function(e) {
+        log.info("Unable to load server config: " + e.message);
+        extConf.loadCallback();
       }
     });
 	});
