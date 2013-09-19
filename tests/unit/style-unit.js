843ebed5258752d65f8d992493bbef21b22fa719
akhabibullina <akhabibullina@aisquared.com>
2013-09-19 11:50:49 -0400

    [cleanup] Fix style test fail.

--- tests/unit/style-unit.js
+++ tests/unit/style-unit.js
@@ -3,6 +3,7 @@
  */
 
 require('./test/bootstrap');
+
 getComputedStyle = null;
 
 // Require the module file we want to test.
@@ -11,6 +12,11 @@ var style = require("../../source/js/style");
 describe('sitecues', function() {
       describe('style', function() {
        it('getComputed should evaluate to empty with empty DOM object', function(done) {
+          // Create & insert a new element we will later use for tests.
+          var node = document.createElement("p");
+          node.setAttribute("id", "sitecues");
+          document.getElementsByTagName('body')[0].appendChild(node);
+
           var res = style.getComputed(document.getElementById('sitecues'));
           expect(res).to.be.ok;
           expect(res).to.be.an('object');
