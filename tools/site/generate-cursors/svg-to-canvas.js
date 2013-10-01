53b90f9f9e43de1b64b77341b22fdc86c9d438c1
Alistair MacDonald <amacdonald@aisquared.com>
2013-10-01 15:19:08 -0400

    EQ-1064: Update to cursor generation process.

--- tools/site/generate-cursors/svg-to-canvas.js
+++ tools/site/generate-cursors/svg-to-canvas.js
@@ -1,18 +1,71 @@
 (function() {
   'use strict';
 
-  // Canvas context
+  // Global Canvas context
   var c
+  
+  // Offset Scale (SVG to Canvas ratio)
+  , scaleX
+  , scaleY
+  , scaleSqrt
     
-  , expression = {
+  // Regular Experssions for SVG Paths
+  , expression  = {
       pathCommand   : '[mzlhvcqtaMZLHVCQTA][^mzlhvcqtaMZLHVCQTA]+'
     , move          : '[0-9][^,]+'
     , curveTo       : '[0-9][^,|^ ]+'
     }
 
-  , attributeList = {
-      path    : ['d']
-    , common  : ['fill']
+  // Get the arguments for a Path command
+  , processCommand = {
+
+      // Move To (Absolute)
+      M: function (command) {
+        var regex   = new RegExp(expression.move, 'g')
+          , coord   = []
+          , nextCoord
+          ;
+
+        while((nextCoord = regex.exec(command))){
+          coord.push(nextCoord[0]);
+        }
+
+        c.moveTo(coord[0], coord[1]);
+      },
+
+      // Line To (Absolute)
+      L: function (command) {
+        var regex   = new RegExp(expression.move, 'g')
+          , coord   = []
+          , nextCoord
+          ;
+
+        while((nextCoord = regex.exec(command))){
+          coord.push(nextCoord[0]);
+        }
+
+        c.lineTo(coord[0], coord[1]);
+      },
+
+      // Curve To (Absolute:Bezier)
+      C: function (command) {
+        var regex   = new RegExp(expression.curveTo, 'g')
+          , coord   = []
+          , nextCoord
+          ;
+
+        while((nextCoord = regex.exec(command))){
+          coord.push(nextCoord[0]);
+        }
+
+        c.bezierCurveTo(coord[0], coord[1], coord[2], coord[3], coord[4], coord[5]);
+      },
+
+      // Close Path
+      z: function () {
+        c.closePath();
+      }
+
     }
   ;
 
@@ -26,17 +79,66 @@
   }
 
 
+  // Apply SVG attributes
+  function applyAttrs (node) {
+    
+    var fill          = node.getAttribute('fill')
+      , stroke        = node.getAttribute('stroke')
+      , strokeWidth   = node.getAttribute('stroke-width')
+      
+      // HTML5 Canvas Based Blur (not saved as raster that gets pixelated when scaled)
+      , shadow        = node.getAttribute('shadow')
+      , shadowBlur    = node.getAttribute('shadow-blur')
+      , shadowOffsetX = node.getAttribute('shadow-offset-x')
+      , shadowOffsetY = node.getAttribute('shadow-offset-y')
+      ;
+
+    if (shadow) {
+      c.shadowColor   = shadow;
+      c.shadowBlur    = shadowBlur * Math.sqrt(scaleX * scaleY);
+      c.shadowOffsetX = shadowOffsetX * scaleX;
+      c.shadowOffsetY = shadowOffsetY * scaleY;
+    } else {
+      c.shadowColor   = '';
+      c.shadowBlur    = 0;
+      c.shadowOffsetX = 0;
+      c.shadowOffsetY = 0;
+    }
+
+    if (fill) {
+      c.fillStyle = fill;
+      c.fill();
+    } else {
+      c.filleStyle = '';
+    }
+
+    if (stroke) {
+      c.strokeStyle = stroke * scaleSqrt;
+      c.stroke();
+    } else {
+      c.strokeStyle = '';
+    }
+
+  }
+
   // Deal with an SVG Path element
   function path (node) {
 
     var regex   = new RegExp(expression.pathCommand, 'g')
       , pathd   = node.getAttribute('d')
-      , command
+      , nextCommand
+      , commandType
       ;
 
-    while((command = regex.exec(pathd))){
-      console.log(command);
+    c.beginPath();
+
+    while((nextCommand = regex.exec(pathd))){
+      commandType = nextCommand[0][0];
+      processCommand[commandType](nextCommand);
     }
+
+    c.closePath();
+    applyAttrs(node);
 
   }
 
@@ -59,26 +161,61 @@
       case 'defs':
         break;
       case 'g':
+        parse(childNode);
         break;
       case 'path':
         path(childNode);
         break;
       }
     }
+
   }
 
+
+  // Get a data URL from the canvas
+  function getDataURL () {
+    return '';
+  };
+
+
   // Public Interface for svg2canvas features
-  window.svg2canvas = function (file, canvasId) {
+  window.svg2canvas = function (file, canvasId, pixelDensity, setWidth, setHeight, callback) {
 
-    var response = load(file)
-      , canvas   = document.getElementById(canvasId)
-      , svg      = response.getElementsByTagName('svg')[0]
+    // TODO:
+    // Convert argument list to object list 
+    // If a canvas is not provided, return the canvas
+
+    var response    = load(file)
+      , canvas      = document.getElementById(canvasId)
+      , svg         = response.getElementsByTagName('svg')[0]
+      , svgWidth    = svg.getAttribute('width')
+      , svgHeight   = svg.getAttribute('height')
+      , width       = setWidth  || svgWidth
+      , height      = setHeight || svgHeight
+      , toDataURL
+      , result
       ;
+    
+    scaleX = width / svgWidth * pixelDensity;
+    scaleY = height / svgHeight * pixelDensity;
+    scaleSqrt = Math.sqrt(scaleX * scaleY);
 
+    canvas.setAttribute('width', width * pixelDensity);
+    canvas.setAttribute('height', height * pixelDensity);
     c = canvas.getContext('2d');
 
+    c.save();
+    c.scale( scaleX, scaleY);
     parse(svg);
+    c.restore();
 
+    canvas.style.width =  width  + 'px';
+    canvas.style.height = height + 'px';
+
+    result = toDataURL ? getDataURL() : canvas;
+
+    return callback ? callback(result) : result;
+  
   };
 
 })();
