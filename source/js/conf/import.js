b9db75a4037d224adba6217484a5126fe3c365a7
Alistair MacDonald <amacdonald@aisquared.com>
2013-10-02 14:41:15 -0400

    EQ-956: Added work-around method in conf that allows sitecues to start with the new sitecues.config{} object method (not yet implemented in the extension), and the old _sitecues[] array method, which will be depreciated in the future. When this is depreciated in the extension, we need to remove the old code in the conf/import.js module.

--- source/js/conf/import.js
+++ source/js/conf/import.js
@@ -1,41 +1,81 @@
 // module for importing settings from page's _sitecues variable
 // needed for customer's identification and customer's settings
 sitecues.def('conf/import', function (module, callback, log) {
+  'use strict';
 
-    // depends on conf module
-    sitecues.use('conf/main', function(conf_main) {
+  // depends on conf module
+  sitecues.use('conf/main', function(conf_main) {
 
-        // push setting up to conf
-        var push = function(data) {
-            // there is no data passed
-            if (!data || !data.length) return;
+    // push setting up to conf
+    function push (data) {
+      
+      // there is no data passed
+      if (!data || !data.length) {
+        return;
+      }
 
-            // get conf key
-            var key = data.shift();
+      // get conf key
+      var key = data.shift();
 
-            // of only one value remains
-            // in data use it as value
-            if (data.length === 1) data = data[0];
+      // of only one value remains
+      // in data use it as value
+      if (data.length === 1) {
+        data = data[0];
+      }
 
-            // export to conf
-            conf_main.set(key, data);
-        }
+      // export to conf
+      conf_main.set(key, data);
+    }
 
-        // settings on the page found
-        if ('_sitecues' in window) {
-            // iterate over them and push to conf
-            for (var i = 0, l = _sitecues.length; i < l; i++)
-                push(_sitecues[i]);
+
+    var i
+      , l
+      , key
+      , sitecuesConfig
+      ;
+
+    // settings on the page found
+    if (!('_sitecues' in window)) {
+      log.warn('_sitecues not defined!');
+
+      if (window.sitecues) {
+        sitecuesConfig = window.sitecues.config;
+        if (sitecuesConfig) {
+          for(key in sitecuesConfig) {
+            if (sitecuesConfig.hasOwnProperty(key)) {
+              conf_main.set(key, window.sitecues.config[key]);
+            }
+          }
         } else {
-            log.warn('_sitecues not defined!');
+          log.warn('Could not find sitecues.config.');
         }
+        if (!window.sitecues.coreConfig.hosts) {
+          log.warn('Could not find sitecues.coreConfig.hosts.');
+        }
+      }
 
-        // replace global var
-        _sitecues = { push: push }
+    } else {
 
-        // done
-        callback();
+      if('_sitecues' in window){
+        
+        console.log("sitecues in window");
+        
+        // iterate over them and push to conf
+        for (i=0, l=_sitecues.length; i < l; i++) {
+          push(_sitecues[i]);
+        }
+      }
 
-    });
+      console.log( _sitecues, _sitecues.length );
+
+      // replace global var
+      _sitecues = { push: push };
+    }
+
+
+    // done
+    callback();
+
+  });
 
 });
