4a15702a95d49b393f5160dff22bc29c068f8162
akhabibullina <akhabibullina@aisquared.com>
2013-10-02 11:33:40 +0300

    EQ-900 Add cursor/images/manager.js to repo.

--- source/js/cursor/images/manager.js
+++ source/js/cursor/images/manager.js
@@ -12,7 +12,7 @@ sitecues.def('cursor/images/manager', function (images, callback) {
     var systemDetect = {
       'OS': navigator.platform || "Unknown OS"
     };
-    
+
     var systemOS = {
       'Windows': 'WIN',
       'Mac': 'MAC',
@@ -33,6 +33,11 @@ sitecues.def('cursor/images/manager', function (images, callback) {
     // Get dependencies: either images for Windows, or for MacOs.
     sitecues.use('cursor/images/' + imageModuleName, function (osImages) {
        images.urls = osImages.urls;
+
+       // Export manager object for unit testing purposes.
+        if (sitecues.tdd) {
+          exports.manager = images;
+        }
 
         // Done.
         callback();
