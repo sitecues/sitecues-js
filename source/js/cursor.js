09597149bca36b45bda3a29e25536deb7179e552
Anthony Nardi <anardi@aisquared.com>
2013-10-25 16:27:30 -0400

    EQ-1033 Removing unused public facing methods, conforming to jsHint standards, slight refactoring

--- source/js/cursor.js
+++ source/js/cursor.js
@@ -15,37 +15,40 @@ sitecues.def('cursor', function (cursor, callback, log) {
     var stylesheetElement,
         stylesheetObject,
         lastZoom = conf.get('zoom'),
-        lastZoomTimeout,
-        DEFAULT_ZOOM_LEVEL = 1,
-        DEFAULT_MIN_ZOOM_LEVEL = 1.1,
-        DEFAULT_TYPE = 'default',
-        SITECUES_CSS_ID = 'sitecues-css',
-        SITECUES_CSS_DEFAULT =         
-        "* {cursor:auto}\n" +
-        "input[type='submit'], input[type='radio'], input[type='button'], input[type='checkbox'], input[type='image'], select, label, a *, a, a:link, a:hover, iframe a, button {cursor:pointer}\n" +
-        "input[type='text'], input[type='email'], input[type='search'] {cursor:text}\n" +
-        "p, textarea {cursor:text}\n" +
-        "#sitecues-panel, .sitecues-badge {cursor:default}\n" +
-        "#sitecues-panel .tts {cursor:pointer}\n" +
-        "#sitecues-close-button {cursor:pointer}\n" +                
-        ".dropdown-menu > .disabled > a:focus {cursor:default}\n" +
-        ".sitecues-slider {cursor:pointer}\n" +
-        ".sitecues-toolbar, .hori {cursor:default}\n" +
-        ".sitecues-slider-thumb {cursor:pointer}\n" +
-        ".sitecues-toolbar .slider-wrap * {cursor:pointer}\n" +
-        ".sitecues-toolbar svg * {cursor:pointer}\n" +
-        ".slider-wrap svg * {cursor:pointer}\n" +
-        ".sitecues-toolbar .tts {cursor:pointer}\n" +
-        ".sitecues-toolbar.hori .dropdown-wrap .dropdown-menu > li > a {cursor:pointer}\n" +
-        ".sitecues-toolbar.hori .dropdown-toggle {cursor:pointer}\n";
-
-    function createCORSRequest(method, url) {
+        lastZoomTimeout;
+    
+    cursor.CONTANTS = {
+      'DEFAULT_ZOOM_LEVEL'     : 1,
+      'DEFAULT_MIN_ZOOM_LEVEL' : 1.1,
+      'DEFAULT_TYPE'           : 'default',
+      'SITECUES_CSS_ID'        : 'sitecues-css',
+      'SITECUES_CSS_DEFAULT'   :         
+        '* {cursor:auto}\n' +
+        'input[type="submit"], input[type="radio"], input[type="button"], input[type="checkbox"], input[type="image"], select, label, a *, a, a:link, a:hover, iframe a, button {cursor:pointer}\n' +
+        'input[type="text"], input[type="email"], input[type="search"] {cursor:text}\n' +
+        'p, textarea {cursor:text}\n' +
+        '#sitecues-panel, .sitecues-badge {cursor:default}\n' +
+        '#sitecues-panel .tts {cursor:pointer}\n' +
+        '#sitecues-close-button {cursor:pointer}\n' +                
+        '.dropdown-menu > .disabled > a:focus {cursor:default}\n' +
+        '.sitecues-slider {cursor:pointer}\n' +
+        '.sitecues-toolbar, .hori {cursor:default}\n' +
+        '.sitecues-slider-thumb {cursor:pointer}\n' +
+        '.sitecues-toolbar .slider-wrap * {cursor:pointer}\n' +
+        '.sitecues-toolbar svg * {cursor:pointer}\n' +
+        '.slider-wrap svg * {cursor:pointer}\n' +
+        '.sitecues-toolbar .tts {cursor:pointer}\n' +
+        '.sitecues-toolbar.hori .dropdown-wrap .dropdown-menu > li > a {cursor:pointer}\n' +
+        '.sitecues-toolbar.hori .dropdown-toggle {cursor:pointer}\n'
+    };
+    
+    function createRequest(method, url) {
       //Credit to Nicholas Zakas 
       var xhr = new XMLHttpRequest();
       
-      if ("withCredentials" in xhr) {
+      if ('withCredentials' in xhr) {
         xhr.open(method, url, true);
-      } else if (typeof XDomainRequest != "undefined") {
+      } else if (typeof XDomainRequest !== 'undefined') {
         xhr = new XDomainRequest();
         xhr.open(method, url);
       } else {
@@ -54,7 +57,7 @@ sitecues.def('cursor', function (cursor, callback, log) {
       return xhr;
     }
 
-    cursor.getStylesheets = function () {
+    function getStylesheets () {
       
       var stylesheets = [],
           linkTags = document.getElementsByTagName('link');
@@ -72,26 +75,11 @@ sitecues.def('cursor', function (cursor, callback, log) {
       
       return stylesheets;
     
-    };
+    }
 
-    cursor.getDomainStylesheets = function () {
+    function createCORSRequest (method, url, callback) {
       
-      var stylesheets = cursor.getStylesheets(),
-          domainStyleSheets = [];
-
-      for(var i = 0; i < stylesheets.length; i += 1) {
-        if (stylesheets[i].indexOf(document.location.host) !== -1) {
-          domainStyleSheets.push(stylesheets[i]);
-        }
-      }
-
-      return domainStyleSheets;
-
-    };
-
-    cursor.createCORSRequest = function (method, url, callback) {
-      
-      var request = createCORSRequest(method, url);
+      var request = createRequest(method, url);
       
       request.url = url;
 
@@ -102,21 +90,16 @@ sitecues.def('cursor', function (cursor, callback, log) {
       request.onload = function () {
         callback(request);
       };
-      
-      request.onerror = function () {
-        //console.log('%c CORS request for ' + request.url + ' failed', 'color:red;background:#ccc');
-        //throw new Error('Error making the CORS request');
-      }
 
       request.send();
 
-    };
+    }
 
     /*
       @param style - String specifying what style we are interested in
       @param callback - Function that gets passed ...
     */
-    cursor.changeStyle = function (style, callback) {
+    function changeStyle (style, callback) {
       var rule;
       if (stylesheetObject) {
         for(var i = 0, rules = stylesheetObject.cssRules; i < rules.length; i += 1) {
@@ -129,14 +112,14 @@ sitecues.def('cursor', function (cursor, callback, log) {
             }
           }
         }
-        if (lastZoom < DEFAULT_MIN_ZOOM_LEVEL) {
+        if (lastZoom < cursor.CONTANTS.DEFAULT_MIN_ZOOM_LEVEL) {
           //if the current zoom level is less than the minimum needed to enable custom cursors...
           stylesheetObject.disabled = true;
         } else {
           stylesheetObject.disabled = false;
         }
       }
-    };
+    }
     
     var createStyleSheet = (function () {
 
@@ -147,10 +130,10 @@ sitecues.def('cursor', function (cursor, callback, log) {
         var cursorTypeURLS = [];
         //generate cursor images for every cursor type...      
         for(var i = 0; i < cursorTypes.length; i += 1) {
-          cursorTypeURLS[cursorTypes[i]] = cursor.generateCursorStyle(cursorTypes[i], lastZoom);
+          cursorTypeURLS[cursorTypes[i]] = generateCursorStyle(cursorTypes[i], lastZoom);
         }
         
-        cursor.changeStyle('cursor', function (rule, style) {
+        changeStyle('cursor', function (rule, style) {
         //find the cursor type (auto, crosshair, etc) and replace the style with our generated image 
           for (var i = 0; i < cursorTypes.length; i += 1) {
             if (rule && rule[style].indexOf(cursorTypes[i]) > -1) {
@@ -167,12 +150,24 @@ sitecues.def('cursor', function (cursor, callback, log) {
             } 
           }        
         });
-        
-      }
+      
+      };
 
     }());
     
-    cursor.generateCursorStyle = function (type, zoom) {
+    function setStyleSheetObject () {
+      stylesheetObject = (function () {
+        for (var i = 0; i < document.styleSheets.length; i += 1) {
+          if (document.styleSheets[i].ownerNode && document.styleSheets[i].ownerNode.id === cursor.CONTANTS.SITECUES_CSS_ID) {
+            return document.styleSheets[i];
+          }
+        }
+      }());
+      lastZoom = conf.get('zoom');
+      createStyleSheet();
+    }
+
+    function generateCursorStyle (type, zoom) {
       return 'url(' + view.getImage(type, zoom) + ') ' + getCursorHotspotOffset(type, zoom) + ', ' + type;
     }
     // EQ-723: Cursor URLs have offset for their hotspots. Let's add the coordinates, using CSS 3 feature.
@@ -186,31 +181,34 @@ sitecues.def('cursor', function (cursor, callback, log) {
      */
     function getCursorHotspotOffset(type, zl) {
        
-       var zoom = {
-         'min': DEFAULT_ZOOM_LEVEL,
-         'current': zl || conf.get('zoom') || DEFAULT_ZOOM_LEVEL,
-       };
+      var zoom = {
+        'min': cursor.CONTANTS.DEFAULT_ZOOM_LEVEL,
+        'current': zl || conf.get('zoom') || cursor.CONTANTS.DEFAULT_ZOOM_LEVEL,
+      },
+
+      offset,
+      result;
        
-       zoom.diff = zoom.current - zoom.min;
+      zoom.diff = zoom.current - zoom.min;
        
-       var offset = imagesManager.offsets[type || DEFAULT_TYPE];
+      offset = imagesManager.offsets[type || cursor.CONTANTS.DEFAULT_TYPE];
        
-       var result = '';
+      result = '';
        
-       if (offset) {
-          switch (type) {
-           case 'auto':
-           case 'default':
-             result = offset.x + ' ' + Math.round(offset.y + offset.step * zoom.diff);
-             break
-           case 'pointer':
-             result = Math.round(offset.x + offset.step * zoom.diff) + ' ' + Math.round(offset.y + (offset.step / 2) * zoom.diff);
-             break;
-           default:
-             break;
-         }
-       }
-       return result;
+      if (offset) {
+        switch (type) {
+        case 'auto':
+        case 'default':
+          result = offset.x + ' ' + Math.round(offset.y + offset.step * zoom.diff);
+          break;
+        case 'pointer':
+          result = Math.round(offset.x + offset.step * zoom.diff) + ' ' + Math.round(offset.y + (offset.step / 2) * zoom.diff);
+          break;
+        default:
+          break;
+        }
+      }
+      return result;
     }    
     
     (function () {  //initializer
@@ -221,58 +219,33 @@ sitecues.def('cursor', function (cursor, callback, log) {
         downloaded, then we simply concatenate our <style> with the response text.
         At the end of each successful callback, we update our <style> to reflect the current level of zoom.
       */
-      var validSheets = cursor.getStylesheets(),
+      var validSheets = getStylesheets(),
           styleTags = document.getElementsByTagName('style'),
           sheet = document.createElement('style');
       
-      sheet.innerHTML = SITECUES_CSS_DEFAULT;
-      sheet.id        = SITECUES_CSS_ID;
+      sheet.innerHTML = cursor.CONTANTS.SITECUES_CSS_DEFAULT;
+      sheet.id        = cursor.CONTANTS.SITECUES_CSS_ID;
       
       document.head.appendChild(sheet);
       
-      stylesheetElement = document.getElementById(SITECUES_CSS_ID);
+      stylesheetElement = document.getElementById(cursor.CONTANTS.SITECUES_CSS_ID);
 
       for(var k = 0; k < styleTags.length; k += 1) {
-        if (styleTags[k].id !== SITECUES_CSS_ID) {
+        if (styleTags[k].id !== cursor.CONTANTS.SITECUES_CSS_ID) {
           stylesheetElement.innerHTML += styleTags[k].innerHTML;
         }
       }
 
       for(var i = 0; i < validSheets.length; i += 1) {
 
-        cursor.createCORSRequest('GET', validSheets[i], function (request) {
-          
-          //console.log('%c CORS Successful for ' + request.url, 'color:green;background:#ccc');
-
-          stylesheetElement.innerHTML += request.responseText;;
-          setTimeout(function () {
-            //Hmm, interesting that I needed to do this...
-            stylesheetObject = (function () {
-              for (var i = 0; i < document.styleSheets.length; i += 1) {
-                if (document.styleSheets[i].ownerNode && document.styleSheets[i].ownerNode.id === SITECUES_CSS_ID) {
-                  return document.styleSheets[i];
-                }
-              }
-            }());
-            lastZoom = conf.get('zoom');
-            createStyleSheet();
-          }, 1);
+        createCORSRequest('GET', validSheets[i], function (request) {
+          stylesheetElement.innerHTML += request.responseText;
+          setTimeout(setStyleSheetObject, 1);
         });
 
       } 
        
-      setTimeout(function () {
-        //Hmm, interesting that I needed to do this...
-        stylesheetObject = (function () {
-          for (var i = 0; i < document.styleSheets.length; i += 1) {
-            if (document.styleSheets[i].ownerNode && document.styleSheets[i].ownerNode.id === SITECUES_CSS_ID) {
-              return document.styleSheets[i];
-            }
-          }
-        }());
-        lastZoom = conf.get('zoom');
-        createStyleSheet();
-      }, 1);
+      setTimeout(setStyleSheetObject, 1);
 
     }());
     
@@ -282,7 +255,7 @@ sitecues.def('cursor', function (cursor, callback, log) {
         clearTimeout(lastZoomTimeout);
         lastZoomTimeout = setTimeout(createStyleSheet, 10);
       }
-    })
+    });
 
     callback();
   
