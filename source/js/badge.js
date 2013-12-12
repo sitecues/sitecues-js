1cd0164eaefe830b6e701842eefed6c62b8d2126
Chris Simari <csimari@aisquared.com>
2013-12-12 11:43:32 -0500

    once again -an attempt to resolve my git issues.

--- source/js/badge.js
+++ source/js/badge.js
@@ -112,19 +112,6 @@ sitecues.def('badge', function (badge, callback, log) {
  
     // BODY
     var $badge = $('#' + badge.badgeId);
-<<<<<<< HEAD
-      if (badge.altBadges && (badge.altBadges.length > 0)) {
-        badge.panel   = badge.altBadges;
-        badge.element = badge.panel;
-      } else if ($badge.length > 0) {
-        badge.panel   = $badge;
-        badge.element = badge.panel;
-      } else {
-        // We have no alternate or pre-existing badges defined, so create a new one.
-        badge.create();
-      }
- 
-=======
 
     if (badge.altBadges && (badge.altBadges.length > 0)) {
       badge.panel   = badge.altBadges;
@@ -137,11 +124,6 @@ sitecues.def('badge', function (badge, callback, log) {
       badge.create();
     }
 
-<<<<<<< HEAD
-      
->>>>>>> 9d37c1f64b193bc6ffbc99f752aff23ab15c4055
-=======
->>>>>>> 181cd6407fb84277c56117689531554db420d31f
     panel.parent  = badge.element;
     $badge = $('#' + badge.badgeId);
 
@@ -151,58 +133,6 @@ sitecues.def('badge', function (badge, callback, log) {
     // When Al MacDonald completes his work, we will probably need to modify it according to his mechanism.
     badge.isBadgeRaplacedByToolbar = isBadgeInDom && $badge.attr(REPLACE_BADGE_ATTR) === 'true';
 
-<<<<<<< HEAD
-    /* EQ-881: As a customer, I want sitecues to degrade gracefully or provide
-    /* a useful fallback when it can't work, so that my users aren't confused by the icon.*/
-    var _requiresFallback = platform.requiresFallback,
-        _supportsTouch = platform.isTouchDevice;
-
-      /* EQ-657 - Handle tablet and smartphone case
-      /* Determine if event was touch based - only limited to event at this level
-      /* 'msgesturechange' is for IE10 - wtf?! */
-      var setFallbackEvents = function (evt){
-                                  evt.preventDefault();
-                                  fallback.show();
-                                };
-      var setDefaultEventOver = function (evt) {
-                                  sitecues.emit('badge/hover', badge.element);
-                                };     
-      var setDefaultEventLeave = function (evt) {
-                                  sitecues.emit('badge/leave', badge.element);
-                                };   
-     
-      // Check if fallbacks are enabled otherwise use default hover
-      if (window.sitecues.getLibraryConfig().fallback.enabled) {
-        console.log('fallbacks are enabled')
-           log.info('fallbacks are enabled');
-            // Delegate Events
-           if( _requiresFallback || _supportsTouch ) { 
-             
-                  switch(_supportsTouch){
-                    case true: 
-                          $(badge.panel).on( 'touchstart' || 'touchmove' || 'touchend' || 'msgesturechange', setFallbackEvents );
-                      break;
-                    case false: 
-                          $(badge.panel).on( 'click', setFallbackEvents );
-                      break;
-                    }
-
-                  if( _requiresFallback && _supportsTouch ){
-                    $(badge.panel).on( 'click' || 'touchstart' || 'touchmove' || 'touchend' || 'msgesturechange', setFallbackEvents );
-                  }
-          
-            }else{
-                // sitecues deployed without need for fallback.',
-                $(badge.panel).hover(setDefaultEventOver, setDefaultEventLeave); 
-              }
-        
-              } else {
-
-                  $(badge.panel).hover(setDefaultEventOver, setDefaultEventLeave);
-                  console.log("Fallback modal is currently disabled.")
-                  log.warn("Fallback modal is currently disabled.");
-                }
-=======
     // EQ-881: As a customer, I want sitecues to degrade gracefully or provide
     // a useful fallback when it can't work, so that my users aren't confused by the icon.
     var setFallbackEvents = function (evt){
@@ -280,7 +210,6 @@ sitecues.def('badge', function (badge, callback, log) {
       $(badge.panel).hover(setDefaultEventOver, setDefaultEventLeave); 
       log.info('Compatibility fallback is not enabled - use sitecues.toggleFalllback() to disable for development.');
     }
->>>>>>> 9d37c1f64b193bc6ffbc99f752aff23ab15c4055
 
     sitecues.on('badge/enable', function() {
       badge.enable(true);
