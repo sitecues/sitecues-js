2241cbf73416d12721e350e5419d78abecad4a88
Alistair MacDonald <amacdonald@aisquared.com>
2014-03-13 15:04:16 -0400

    demoing new build system after standup

---
+++ custom-config/newclient.mk
@@ -0,0 +1,8 @@
+################################################################################
+# The NewClient custom build configuration.
+################################################################################
+
+custom-name=newclient
+custom-files=\
+	source/js/custom-scripts/something-awesome.js
+
