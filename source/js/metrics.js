c9714e6dff8e39a96027b0bafcf0909d2f3493aa
akhabibullina <akhabibullina@aisquared.com>
2014-04-18 14:03:17 +0300

    SC-1626 Remove singleton.

--- source/js/metrics.js
+++ source/js/metrics.js
@@ -36,63 +36,58 @@ sitecues.def('metrics', function(metrics, callback, log) {
     // Taken from here(free puplic license): https://gist.github.com/jed/982883
     var UUIDv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)};
 
-    var instance = null;
+    sitecues.use('metrics/util', 'jquery', 'conf', 'conf/site', 'conf/user', 'speech', 'zoom', 'ui',
+            function(metricsUtil, $, conf, site, user, speech, zoom) {
 
-    sitecues.use('metrics/util', 'jquery', 'conf', 'conf/site', 'speech', 'ui',
-        function(metricsUtil, $, conf, site, speech) {
-            var Metrics = (function() {
-                // Constructor.
-                function Metrics() {
-                    // todo: extend with pre-defined state.
-                    this.options = {};
+                // ============= Objects methods ======================
+                metrics.init = function() {
                     // Default state.
-                    this.data = $.extend({}, DEFAULT_STATE);
-                    // Initialize.
-                    // A random UUID v4 generated for this library session.
-                    this.data.session_id = UUIDv4();
-                    // Epoch time in milliseconds  when the event occurred
-                    this.data.client_time_ms = +new Date;
-                    this.data.page_url = location && location.href? location.href: '';
-                    this.data.zoom_level = conf.get('zoom') || 1;
-                    this.data.tts_state = !site.get('ttsAvailable')
-                                          ? TTS_STATES['unavailable']
-                                          // 1 - for 'enabled', 0 - for 'disabled'
-                                          : +speech.isEnabled();
-                    this.data.browser_user_agent = navigator && navigator.userAgent ? navigator.userAgent : '';
-                    this.data.client_language = navigator && navigator.language ? navigator.language: '';
-                    sitecues.emit('metrics/ready', this, $.extend(true, {}, this.options));
+                    metrics.data = $.extend({}, DEFAULT_STATE);
+                    // Initialization.
+                    var data = {
+                        // A random UUID v4 generated for this library session.
+                        'session_id': UUIDv4(),
+                        // Epoch time in milliseconds  when the event occurred
+                        'client_time_ms': +new Date,
+                        'page_url'  : location && location.href ? location.href : '',
+                        'zoom_level': conf.get('zoom') || 1,
+                        'tts_state' : !site.get('ttsAvailable')
+                                     ? TTS_STATES['unavailable']
+                                     // 1 - for 'enabled', 0 - for 'disabled'
+                                     : +speech.isEnabled(),
+                        'browser_user_agent': navigator && navigator.userAgent ? navigator.userAgent : '',
+                        'client_language': navigator && navigator.language ? navigator.language : ''
+                    };
+                    metrics.update(data);
+                    sitecues.emit('metrics/ready', metrics);
                 };
 
-                // Singleton.
-                return {
-                    createInstance: function(options) {
-                        return (new Metrics(options) || null);
-                    },
-                    updateInstance: metricsUtil.update
+                metrics.update = function(data) {
+                    if (!metrics['data']) {
+                        metrics.init();
+                    }
+                    metricsUtil.update(metrics, data, 'metrics/update');
                 };
-            })();
-            
-            instance = Metrics.createInstance();
 
-            sitecues.on('zoom', function(zoomLevel) {
-                // Update zoom.
-                var data = {'zoom_level': parseFloat(zoomLevel)};
-                Metrics.updateInstance(instance, data, 'metrics/update');
+                // ============= Events Handlers ======================
+                sitecues.on('zoom', function(zoomLevel) {
+                    var data = {'zoom_level': parseFloat(zoomLevel)};
+                    metrics.update(data);
+                });
+
+                sitecues.on('speech/enable speech/disable', function() {
+                    var ttsState = conf.get('siteTTSEnable') ? TTS_STATES['enabled'] : TTS_STATES['disabled'];
+                    var data = {'tts_state': ttsState};
+                    metrics.update(data);
+                });
+
+                // Update the basic metrics when panel is showed.
+                sitecues.on('metrics/panel-closed/create metrics/badge-hovered/create metrics/hlb-opened/create', function() {
+                    var data = {'client_time_ms': +new Date};
+                    metrics.update(data);
+                });
+
+                // Done.
+                callback();
             });
-
-            sitecues.on('speech/enable speech/disable', function() {
-                var ttsState = conf.get('siteTTSEnable') ? TTS_STATES['enabled'] : TTS_STATES['disabled'];
-                var data = {'tts_state': ttsState};
-                Metrics.updateInstance(instance, data, 'metrics/update');
-            });
-
-            // Update the basic metrics when panel is showed.
-            sitecues.on('metrics/panel-closed/create metrics/badge-hovered/create metrics/hlb-opened/create', function() {
-                var data = {'client_time_ms': +new Date};
-                Metrics.updateInstance(instance, data, 'metrics/update');
-            });
-
-            // Done.
-            callback();
-    });
 });
