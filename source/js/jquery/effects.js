f530f5eefcb984ef90ad94140d59f1c051d1c74e
akhabibullina <akhabibullina@aisquared.com>
2014-05-05 19:14:14 +0300

    SC-1653 First try on optional animations(use effects.doAnimate to switch from animation to regular view).

---
+++ source/js/jquery/effects.js
@@ -0,0 +1,48 @@
+sitecues.def('jquery/effects', function(effects, callback, log) {
+    'use strict';
+
+    sitecues.use('jquery', function($) {
+
+        var toClass = {}.toString;
+
+        $.fn.effects = function(cssStyle, duration, easing, complete) {
+
+            // DOM node
+            var node = this.get(0),
+                    doAnimate = false,
+                    type;
+
+            // Ensure we have a DOM node 
+            if (node === undefined) {
+                return;
+            }
+
+            type = toClass.call(cssStyle).slice(8, -1) || '';
+
+            if (type === 'Object') {
+                // Default values.
+                var duration = duration || 400,
+                    easing   = easing   || 'swing',
+                    complete = complete || undefined;
+
+                // Animations...
+                if (doAnimate) {
+                    $(this).animate(cssStyle, duration, easing, complete);
+                } else {
+                    // Simply apply the style.
+                    $(this).css(cssStyle);
+                    if (complete) {
+                        complete();
+                    }
+                }
+            }
+
+            return this;
+        };
+
+
+        callback();
+
+    });
+
+});
