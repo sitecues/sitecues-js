b9f49e12a821e60968b63a3c7c8cc8e6a8488d3e
akhabibullina <akhabibullina@aisquared.com>
2014-05-30 10:51:35 -0400

    Change the data type.

--- source/js/cursor.js
+++ source/js/cursor.js
@@ -211,7 +211,7 @@ sitecues.def('cursor', function (cursor, callback) {
                         url: cursorValueURL,
                         crossDomain: true,
                         beforeSend: function(xhrObj){
-                            // xhrObj.setRequestHeader("Accept", "application/json"); // application/octet-stream
+                           xhrObj.setRequestHeader("Accept", "application/octet-stream");
                         },
                         type: "GET",
                         async: true,
@@ -219,7 +219,7 @@ sitecues.def('cursor', function (cursor, callback) {
                         success: function(data, status, xhr) {
                             console.log('Loading of CUR file completed!');
                             // console.log(JSON.stringify(data));
-                            // rule.style.setProperty('cursor', 'url(' +cursorValueURL+ '), ' + cursorTypes[i]);
+                            rule.style.setProperty('cursor', data);
                         },
                         error: function() {
                             console.log("Unable to fetch cursor image from server");
