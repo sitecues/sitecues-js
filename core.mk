a244b18974add11d6878f9a86fde8f9ff06f8cfc
Aaron Leventhal <aleventhal@aisquared.com>
2014-05-30 15:59:11 -0400

    Remove unused sitepicker.js

--- core.mk
+++ core.mk
@@ -94,7 +94,6 @@ files=\
 	source/js/hpan.js \
 	source/js/util/template.js \
 	source/js/status.js \
-	source/js/sitepicker.js \
 	source/js/metrics/util.js \
 	source/js/metrics/page-visited.js \
 	source/js/metrics/panel-closed.js \
