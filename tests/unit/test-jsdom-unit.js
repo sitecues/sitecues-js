be5d782484306d2831d31ebc7fff2ad726f02192
akhabibullina <akhabibullina@aisquared.com>
2014-06-06 15:09:44 -0400

    More improvements in unit tests.

--- tests/unit/test-jsdom-unit.js
+++ tests/unit/test-jsdom-unit.js
@@ -1,32 +1,30 @@
 require('./test/bootstrap');
-var page,
-    fs = require('fs');
+var page, fs = require('fs');
 
 // #1
-fs.readFile('./data/html/test-page.html', 'utf8', function(err, file) {
-    var page = file;
-    jsdom.env(
-            page, [jquery],
-            function(err, window) {
-                if (err) {
-                    console.log(err);
-                }
-                var document = window.document;
-                console.log(window.document.documentElement.innerHTML);
-                console.log(document.documentElement.innerHTML);
-                //console.log(jquery(document.documentElement).innerHTML);
-                //console.log(jquery(page).find('p').length);
-                
-                // ---------- TESTS -----------
-                describe('test', function() {
-                    describe('#loadMarkup()', function() {
-                        it('should load markup.', function(done) {
-                            // Use window.document or document for testing...
-                            done();
-                        });
-                    });
+describe('test', function() {
+    describe('#loadMarkup()', function() {
+        before(function() {
+            function tweakIt(html_in, callback) {
+                var jsdom = require('jsdom');
+                jsdom.env({
+                    html: html_in,
+                    scripts: [],
+                    done: function(errors, window) {
+                        callback(jquery(window.document).find('div')[0].innerHTML);
+                    }
                 });
+            }
+
+            var oldhtml = fs.readFileSync('./data/html/test-page.html');
+            var newhtml = tweakIt(oldhtml, function(newstuff) {
+              console.log(newstuff); // woohoo! it works!
             });
+        });
+        it('should load markup.', function(done) {
+            done();
+        });
+    });
 });
 
 // #2
@@ -34,12 +32,11 @@ fs.readFile('./data/html/test-page.html', 'utf8', function(err, file) {
     page = file;
 
     describe('test', function() {
-        describe('#loadMarkup()', function() {
+        describe('#loadMarkup() - 2', function() {
             it('should load markup.', function(done) {
-                console.log(jquery(page));
+                console.log(jquery(page).find('div')[0].innerHTML);
                 done();
             });
         });
     });
 });
-
