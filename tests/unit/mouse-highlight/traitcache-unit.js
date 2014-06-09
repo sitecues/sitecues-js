ddb8f3007440ffa312601c8cf4c7f8fff86945db
Aaron Leventhal <aleventhal@aisquared.com>
2014-06-09 11:22:51 -0400

    Address Anna's review comments

--- tests/unit/mouse-highlight/traitcache-unit.js
+++ tests/unit/mouse-highlight/traitcache-unit.js
@@ -12,13 +12,13 @@ getComputedStyle = function() {
 
 describe('traitcache', function() {
   describe('#getUniqueId()', function() {
-    it('should provide a number.', function (done) {
+    it('should provide a unique ID number for a given HTML element.', function (done) {
       var divElement = document.createElement('div'),
         id = traitcache.getUniqueId(divElement);
       expect(typeof id).to.be.equal('number');
       done();
     });
-    it('should always provide the same id for a given element.', function (done) {
+    it('should always provide the same id for the same given HTML element.', function (done) {
       var divElement = document.createElement('div'),
         id = traitcache.getUniqueId(divElement),
         id2 = traitcache.getUniqueId(divElement);
@@ -47,72 +47,103 @@ describe('traitcache', function() {
   });
 
   describe('#getStyle()', function() {
-    it('should return computed style.', function (done) {
+    it('should return computed style for HTML element.', function (done) {
       var divElement = document.createElement('div'),
-        style,
-        style2;
+        actualStyle, expectedStyle;
 
-      style = traitcache.getStyle(divElement);
-      style2 = getComputedStyle(divElement);
-      expect(JSON.stringify(style)).to.be.equal(JSON.stringify(style2));
-      style = traitcache.getStyle(divElement);
-      expect(JSON.stringify(style)).to.be.equal(JSON.stringify(style2));
+      actualStyle = traitcache.getStyle(divElement);
+      expectedStyle = getComputedStyle(divElement);
+      expect(JSON.stringify(actualStyle)).to.be.equal(JSON.stringify(expectedStyle));
+      done();
+    });
+    it('should return same result when retrieving from cache.', function (done) {
+      var divElement = document.createElement('div'),
+        fetchedStyle, fetchedCachedStyle;
+
+      fetchedStyle = traitcache.getStyle(divElement);
+      fetchedCachedStyle = traitcache.getStyle(divElement);  // Second call will exercise cache
+      expect(JSON.stringify(fetchedCachedStyle)).to.be.equal(JSON.stringify(fetchedStyle));
       done();
     });
   });
   describe('#getStyleProp()', function() {
-    it('should return valid style properties.', function (done) {
+    it('should return valid style properties for HTML element.', function (done) {
       var divElement = document.createElement('div'),
         borderTop;
 
       borderTop = traitcache.getStyleProp(divElement, 'borderTop');
       expect(borderTop).to.be.equal('3px');
-      borderTop = traitcache.getStyleProp(divElement, 'borderTop');
-      expect(borderTop).to.be.equal('3px'); // Second iteration exercises caching mechanism
+      done();
+    });
+    it('should return same result when retrieving from cache.', function (done) {
+      var divElement = document.createElement('div'),
+        borderTopFromCache;
+
+      traitcache.getStyleProp(divElement, 'borderTop');
+      borderTopFromCache = traitcache.getStyleProp(divElement, 'borderTop');
+      expect(borderTopFromCache).to.be.equal(borderTop); // Second iteration exercises caching mechanism
       done();
     });
   });
   describe('#getRect()', function() {
-    it('should always return the correct rectangle.', function (done) {
+    it('should return the correct rectangle for HTML element, in document-relative coordinates.', function (done) {
       var divElement = document.createElement('div'),
-        rect, rect2;
+        actualRect,
+        FAKE_RECT = { top: 1, left: 1, width: 1, height: 1, bottom: 2, right: 2 },
+        FAKE_SCROLL_X = 10,
+        FAKE_SCROLL_Y = 10;
       divElement.getBoundingClientRect = function() {
-        return { top: 1, left: 1, width: 1, height: 1, bottom: 2, right: 2 };
-      }
-      window.pageXOffset = 10;
-      window.pageYOffset = 10;
+        return FAKE_RECT;
+      };
+      window.pageXOffset = FAKE_SCROLL_X;
+      window.pageYOffset = FAKE_SCROLL_Y;
       traitcache.checkViewHasChanged(); // Need to do this so getRect() can compute absolute cooordinates
-      rect = traitcache.getRect(divElement);
-      rect2 = traitcache.getRect(divElement);
-      expect(rect.left).to.be.equal(11);
-      expect(rect.top).to.be.equal(11);
-      expect(rect.width).to.be.equal(1);
-      expect(rect.height).to.be.equal(1);
-      expect(rect.right).to.be.equal(12);
-      expect(rect.bottom).to.be.equal(12);
-      expect(rect).to.be.equal(rect2); // Tests caching mechanism
+      actualRect = traitcache.getRect(divElement);
+      expect(actualRect.left).to.be.equal(FAKE_RECT.left + FAKE_SCROLL_X);
+      expect(actualRect.top).to.be.equal(FAKE_RECT.top + FAKE_SCROLL_Y);
+      expect(actualRect.width).to.be.equal(FAKE_RECT.width);
+      expect(actualRect.height).to.be.equal(FAKE_RECT.height);
+      expect(actualRect.right).to.be.equal(FAKE_RECT.right + FAKE_SCROLL_X);
+      expect(actualRect.bottom).to.be.equal(FAKE_RECT.bottom + FAKE_SCROLL_Y);
       done();
     });
   });
   describe('#getScreenRect()', function() {
-    it('should always return the correct rectangle.', function (done) {
+    it('should return the correct rectangle, in screen-relative coordinates.', function (done) {
       var divElement = document.createElement('div'),
-        rect, rect2;
+        actualRect,
+        FAKE_RECT = { top: 1, left: 1, width: 1, height: 1, bottom: 2, right: 2 },
+        FAKE_SCROLL_X = 10,
+        FAKE_SCROLL_Y = 10;
+
+      // Prepare objects for testing
       divElement.getBoundingClientRect = function () {
-        return { top: 1, left: 1, width: 1, height: 1, bottom: 2, right: 2 };
-      }
-      window.pageXOffset = 10;
-      window.pageYOffset = 10;
+        return FAKE_RECT;
+      };
+      window.pageXOffset = FAKE_SCROLL_X;
+      window.pageYOffset = FAKE_SCROLL_Y;
+
       traitcache.checkViewHasChanged(); // Need to do this so getRect() can compute absolute cooordinates
-      rect = traitcache.getScreenRect(divElement);
-      rect2 = traitcache.getScreenRect(divElement);
-      expect(rect.left).to.be.equal(1);
-      expect(rect.top).to.be.equal(1);
-      expect(rect.width).to.be.equal(1);
-      expect(rect.height).to.be.equal(1);
-      expect(rect.right).to.be.equal(2);
-      expect(rect.bottom).to.be.equal(2);
-      expect(JSON.stringify(rect)).to.be.equal(JSON.stringify(rect2)); // Tests caching mechanism
+      actualRect = traitcache.getScreenRect(divElement);
+      expect(JSON.stringify(actualRect)).to.be.equal(JSON.stringify(FAKE_RECT));
+      done();
+    });
+    it('should return the same results when retrieved from cache.', function (done) {
+      var divElement = document.createElement('div'),
+        fetchedRect, fetchedCachedRect,
+        FAKE_RECT = { top: 1, left: 1, width: 1, height: 1, bottom: 2, right: 2 },
+        FAKE_SCROLL_X = 10,
+        FAKE_SCROLL_Y = 10;
+
+      divElement.getBoundingClientRect = function () {
+        return FAKE_RECT;
+      };
+      window.pageXOffset = FAKE_SCROLL_X;
+      window.pageYOffset = FAKE_SCROLL_Y;
+      traitcache.checkViewHasChanged(); // Need to do this so getRect() can compute absolute cooordinates
+      fetchedRect = traitcache.getScreenRect(divElement);
+      fetchedCachedRect = traitcache.getScreenRect(divElement);
+      expect(JSON.stringify(fetchedCachedRect)).to.be.equal(JSON.stringify(fetchedRect));
       done();
     });
   });
@@ -123,3 +154,4 @@ describe('traitcache', function() {
   });
 });
 
+require('../test/discharge');
