5aa7ff41bcd5660d41d8114c810a073a60d5ea98
Alistair MacDonald <amacdonald@aisquared.com>
2014-07-11 13:43:28 -0400

    Adding scitecues-load-script.js to be served to Charles (easier to use JS file for switching between versions etc, than using multiple charles rules).

---
+++ tools/site/load-script/sitecues-load-script.js
@@ -0,0 +1,16 @@
+// DO NOT MODIFY THIS SCRIPT WITHOUT ASSISTANCE FROM SITECUES
+var sitecues = window.sitecues || {};
+  
+sitecues.config = {
+  site_id : 's-00000005',
+};
+  
+(function () {
+  var script = document.createElement('script'),
+  first = document.getElementsByTagName('script')[0];
+  sitecues.config.script_url=document.location.protocol+'//localhost:8000/l/s;id='+sitecues.config.site_id+'/js/sitecues.js';
+  script.src=sitecues.config.script_url;
+  script.type = 'text/javascript';
+  script.async = true;
+  first.parentNode.insertBefore(script, first);
+})();
