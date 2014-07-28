6a00fe796cd5747fa96c6b7e8becf8fa7b5e1e3f
Aaron Leventhal <aleventhal@aisquared.com>
2014-07-28 14:40:15 -0400

    Not working

---
+++ source/js/custom-scripts/texasat/custom-picker.js
@@ -0,0 +1,19 @@
+/**
+ * Customer-Customization File
+ * CustomId    : custom_a-c27fa71d_SC-1884
+ * IssueLink   : https://equinox.atlassian.net/browse/SC-1884
+ * Description : workaround for ugly HLB, don't pick #right or #rightin
+ * 
+*/
+
+sitecues.def(function (module, callback) {
+  sitecues.use('custom', function (custom) {
+    custom.register('mouse-highlight/picker', function (event) {
+      this.provideCustomSelectors({
+        ignore: '#right,#rightin'
+      });
+    });
+
+    callback();
+  });
+});
