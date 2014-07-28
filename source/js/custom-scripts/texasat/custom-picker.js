7a3f97bb4bf34426092209f5041e7bb35d96c526
Aaron Leventhal <aleventhal@aisquared.com>
2014-07-28 17:40:26 -0400

    Better working workaround for SC-1886

--- source/js/custom-scripts/texasat/custom-picker.js
+++ source/js/custom-scripts/texasat/custom-picker.js
@@ -3,7 +3,7 @@
  * CustomId    : custom_a-c27fa71d_SC-1884
  * IssueLink   : https://equinox.atlassian.net/browse/SC-1884
  * Description : workaround for ugly HLB, don't pick #right or #rightin
- * 
+ *               Also temporary workaround for SC-1886 -- <input type="text"> issues -- don't pick them for now.
 */
 
 sitecues.def(function (module, callback) {
