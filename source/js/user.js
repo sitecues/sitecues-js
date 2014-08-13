358971778adf053466c5e4c1909fbaf2f8ead682
akhabibullina <ascii88@mail.ru>
2014-08-13 19:11:19 +0300

    SC-1847 Do not get user ID or settings from server if already in local storage.

--- source/js/user.js
+++ source/js/user.js
@@ -1,32 +1,38 @@
 // This module includes functionality used in user identification.
-sitecues.def('user', function (user, callback) {
-  var userId = null;
+sitecues.def('user', function(user, callback) {
+
+  var userId = window.localStorage.getItem('userId') || null;
 
   user.getId = function() {
     return userId;
   };
 
-  sitecues.use('jquery', function(jquery) {
-    jquery.ajax({
-      xhrFields: {
-        withCredentials: true
-      },
-      crossDomain: true,
-      beforeSend: function(xhrObj){
-        xhrObj.setRequestHeader("Accept","application/json");
-      },
-      url: '//' + sitecues.getLibraryConfig().hosts.ws + '/sitecues/api/user/id/get.json',
-      type: "GET",
-      success: function(data) {
-        userId = data.userId;
-        callback();
-      },
-      error: function(xhr, textStatus) {
-        if (SC_DEV) {
-          console.log('===== Unable to get user ID: ' + textStatus);
+  if (!userId) {
+    SC_DEV && console.log('UserID  not found in localStorage, fallback to ajax request.');
+    sitecues.use('jquery', function(jquery) {
+      jquery.ajax({
+        xhrFields: {
+          withCredentials: true
+        },
+        crossDomain: true,
+        beforeSend: function(xhrObj) {
+          xhrObj.setRequestHeader("Accept", "application/json");
+        },
+        url: '//' + sitecues.getLibraryConfig().hosts.ws + '/sitecues/api/user/id/get.json',
+        type: "GET",
+        success: function(data) {
+          userId = data.userId;
+          window.localStorage.setItem('userId', data.userId);
+          callback();
+        },
+        error: function(xhr, textStatus) {
+          if (SC_DEV) {
+            console.log('===== Unable to get user ID: ' + textStatus);
+          }
+          callback();
         }
-        callback();
-      }
+      });
     });
-  })
+  }
+
 });
