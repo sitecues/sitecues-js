69149c49f5955dfbded79562e4d80a5deae73444
akhabibullina <ascii88@mail.ru>
2014-10-08 17:59:03 +0300

    Add comment as per Aaron's request to rev 983e34c.

--- source/js/metrics.js
+++ source/js/metrics.js
@@ -62,6 +62,7 @@ sitecues.def('metrics', function (metrics, callback) {
       });
 
       sitecues.on('speech/enabled speech/disabled', function() {
+          // + is a simple way to convert boolean to a number: true becomes 1 and false is 0. I'll add a comment to it.
           var ttsState = +audio.isSpeechEnabled();
           var data = {'tts_state': ttsState};
           metrics.update(data);
