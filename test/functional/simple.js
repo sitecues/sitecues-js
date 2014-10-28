7fd0170d8277505f651325082917ed66a4243468
Seth Holladay <sholladay@aisquared.com>
2014-10-28 13:30:21 -0400

    Temporary workaround for testing on Linux.

--- test/functional/simple.js
+++ test/functional/simple.js
@@ -6,7 +6,7 @@ define(
     ],
     function (tdd, assert, require) {
 
-        var url = 'http://www.sitecues.com'
+        var url = 'http://tools.qa.sitecues.com:9000/site/wrapping.html?scjsurl=//js.dev.sitecues.com/l/s;id=s-00000005/v/dev/latest/js/sitecues.js&scwsid=s-00000005&scuimode=badge&scisv=2';
 
         with (tdd) {
             suite('HLB Simple', function () {
@@ -15,7 +15,7 @@ define(
                     return this.remote               // represents the browser being tested
                         .get(url)                    // navigate to the desired page
                         .setFindTimeout(12000)       // fail test if any find method can't succeed this quickly
-                        .findById('sitecues-panel')
+                        .findById('sitecues-panel')  // signal that sitecues is loaded and ready
                             .pressKeys('\uE025')     // unicode for: hit the + key!
                             .pressKeys('\uE025')
                             .pressKeys('\uE025')
@@ -23,7 +23,7 @@ define(
                             .pressKeys('\uE025')
                             .pressKeys('\uE025')
                             .end()                   // get out of the current element context
-                        .findByCssSelector('span.desc-text-large')
+                        .findByCssSelector('p')      // find the first paragraph
                             .moveMouseTo()
                             .click()                 // give it focus so the picker can detect the mouse
                             .pressKeys('\uE00D')     // hit the spacebar, to open the HLB
@@ -32,7 +32,11 @@ define(
                         .findById('sitecues-hlb')    // get the HLB!
                         .getVisibleText()
                         .then(function (text) {
-                            assert.strictEqual(text, 'Reach and delight more people with zoom & speech built right into your site.', 'The HLB should contain the same text as the picked element.');
+                            assert.strictEqual(
+                                text,
+                                'This is a normal, but long paragraph. I contain multiple sentences and should be as wide as my container. I should wrap as appropriate for my width, no more and no less.',
+                                'The HLB should contain the same text as the picked element.'
+                            );
                         });
                 });
             });
