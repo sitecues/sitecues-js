301ca6679f257fd75c42c6fd1720d4f9b7f93755
Seth Holladay <sholladay@aisquared.com>
2014-10-29 16:33:05 -0400

    Test stability enhancements.

--- test/functional/simple.js
+++ test/functional/simple.js
@@ -13,9 +13,10 @@ define(
 
                 test('HLB Respects Text', function () {
                     return this.remote               // represents the browser being tested
+                        .maximizeWindow()            // use a large window, as a hacky fix to some .moveMouseTo() issues in Firefox
                         .get(url)                    // navigate to the desired page
-                        .setFindTimeout(12000)       // fail test if any find method can't succeed this quickly
-                        .findById('sitecues-panel')  // signal that sitecues is loaded and ready
+                        .setFindTimeout(10000)       // fail test if any find method can't succeed this quickly
+                        .findById('sitecues-panel')  // finding this is our sign that sitecues is loaded and ready
                             .pressKeys('\uE025')     // unicode for: hit the + key!
                             .pressKeys('\uE025')
                             .end()                   // get out of the current element context
@@ -23,10 +24,9 @@ define(
                         .findByCssSelector('#p1')    // find the first paragraph
                             .moveMouseTo()           // scrolls to the element and puts the mouse inside of it
                             .sleep(1000)             // TODO: change to a pollUntil helper, which returns true when the highlighter has finished its work
-                            .click()                 // TODO: figure out why the HLB contains the wrong text in Firefox if we do not do this
                             .pressKeys('\uE00D')     // hit the spacebar, to open the HLB
                             .end()
-                        .setFindTimeout(9000)        // set the find timeout to be more strict
+                        .setFindTimeout(3000)        // set the find timeout to be more strict
                         .findById('sitecues-hlb')    // get the HLB!
                         .getVisibleText()
                         .then(function (text) {
