84067e3d030df3a12ae3713b385fd8e19f6ea3f6
Seth Holladay <sholladay@aisquared.com>
2014-11-07 16:57:43 -0500

    Switch to cleaner method of highlighting before opening the HLB in functional tests.

--- test/functional/simple.js
+++ test/functional/simple.js
@@ -18,28 +18,30 @@ define(
 
                 test('HLB Respects Text', function () {
                     return this.remote               // represents the browser being tested
-                        .maximizeWindow()            // use a large window, as a hacky fix to some .moveMouseTo() issues in Firefox
+                        .maximizeWindow()            // best effort to normalize window sizes (not every browser opens the same)
                         .get(url)                    // navigate to the desired page
-                        .setFindTimeout(9000)       // fail test if any find method can't succeed this quickly
+                        .setFindTimeout(6000)        // fail test if any find method can't succeed this quickly
                         .findById('sitecues-panel')  // finding this is our sign that sitecues is loaded and ready
                             .pressKeys('\uE025')     // unicode for: hit the + key!
                             .pressKeys('\uE025')
                             .end()                   // get out of the current element context
                         .sleep(1200)                 // TODO: change to a pollUntil helper, using sitecue.on('zoom', fn) to return true when zoom is done
                         .findByCssSelector('#p1')    // find the first paragraph
-                            .moveMouseTo()           // scrolls to the element and puts the mouse inside of it
-                            .sleep(1000)             // TODO: change to a pollUntil helper, which returns true when the highlighter has finished its work
-                            .click()                 // hacky workaround to the fact that .moveMouseTo() isn't always enough
-                            .sleep(1000)
+                            .execute(                // run the given code in the remote browser
+                                function () {
+                                    sitecues.highlight('p');
+                                }
+                            )
                             .pressKeys('\uE00D')     // hit the spacebar, to open the HLB
                             .end()
-                        .setFindTimeout(3000)        // set the find timeout to be more strict
+                        .setFindTimeout(2000)        // set the find timeout to be more strict
                         .findById('sitecues-hlb')    // get the HLB!
                         .getVisibleText()
                         .then(function (text) {
                             assert.strictEqual(
                                 text,
-                                '(p1) Dr. Frankenstein\'s grandson, after years of living down the family reputation, inherits granddad\'s castle and repeats the experiments.',
+                                '(p1) Dr. Frankenstein\'s grandson, after years of living down the family ' +
+                                'reputation, inherits granddad\'s castle and repeats the experiments.',
                                 'The HLB should contain the same text as the picked element.'
                             );
                         });
