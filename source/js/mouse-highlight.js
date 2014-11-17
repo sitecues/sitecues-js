11049f367ae45939926f220174af0203db3ebae9
Aaron Leventhal <aleventhal@aisquared.com>
2014-11-17 14:42:54 -0500

    SC-2266. [CIT] ACBOhio highlighting the 'like us on facebook/twitter' icons makes them blank. This checkin makes the original fix work in IE as well, where getComputedStyle does not work with the shorthand background property. r=tony

--- source/js/mouse-highlight.js
+++ source/js/mouse-highlight.js
@@ -416,10 +416,10 @@ sitecues.def('mouse-highlight', function (mh, callback) {
       var newBgPos = (offsetLeft / state.zoom) + 'px '+ (offsetTop / state.zoom) + 'px',
         newBg ='url("data:image/svg+xml,' + encodeURI(svgMarkup) + '") no-repeat ' + newBgPos + ' scroll',
         origStyle = traitcache.getStyle(element),
-        origBg = origStyle.background,
         // Remove color and other properties from the original background string, if they don't go into the shorthand background property
         origBgShorthandProperties = origStyle.backgroundImage !== 'none' &&
-          ',' + origBg.substring(origBg.indexOf(')') + 1, origBg.lastIndexOf('/') ),
+          ',' + origStyle.backgroundImage + ' ' + origStyle.backgroundRepeat + ' ' +
+            origStyle.backgroundAttachment + ' ' + origStyle.backgroundPosition,
         // Put new background behind old one if it exists -- all browsers in our matrix support multiple backgrounds
         compositeBg = newBg + origBgShorthandProperties;
 
