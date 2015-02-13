b684283d0764d70e09b1a732fa89ccf12a7f29e8
Aaron Leventhal <aleventhal@aisquared.com>
2015-02-13 15:36:01 -0500

    SC-2448. Try a box shadow on BP2 to help it stand out from background of page. r=tony, uxr=seth

--- source/js/bp/view/svg.js
+++ source/js/bp/view/svg.js
@@ -36,8 +36,12 @@ sitecues.def('bp/view/svg', function (bpSVG, callback) {
     <path id="scp-outline-def" d="M808 187c0 6-5 11-11 11H11 c-6 0-11-5-11-11V0c0 0 5 0 11 0h786c6 0 11 0 11 0V187" stroke="#989898" stroke-width="1.5"/> \
   </clipPath> \
   <path id="scp-rating-star-def" d="M30 45L48 54L44 35L59 21L39 18L30 0L21 18L1 21L16 35L12 54L30 45"/> \
+  <filter id="scp-shadowblur"> \
+    <feGaussianBlur in="SourceGraphic" stdDeviation="5" /> \
+  </filter> \
 </defs> \
 <g id="scp-main" aria-label="sitecues main panel">\
+  <use id="scp-shadow" xlink:href="#scp-outline-def" x="5" y="5" class="scp-large-only" fill="rgba(0,0,0,.2)" opacity="0" filter="url(#scp-shadowblur)"/>\
   <use id="scp-main-outline" xlink:href="#scp-outline-def" class="scp-large-only" opacity="0" fill="white"/>\
   <use id="scp-small-A" xlink:href="#scp-small-A-def" y="48" role="button" aria-label="Decrease zoom" class="scp-A-button scp-target"/>\
   <g id="scp-zoom-slider-bar" role="slider" aria-valuemin="1" aria-valuemax="3" aria-valuenow="1" aria-labelledby="scp-zoom-label" data-thumb-mover="scp-zoom-slider-thumb" class="scp-target">\
