4a65c228454e72565e03df5d20fbd3c9143b4ebc
Aaron Leventhal <aleventhal@aisquared.com>
2015-03-10 12:43:55 -0400

    Fix tests

--- tests/unit/data/modules/util/localization.js
+++ tests/unit/data/modules/util/localization.js
@@ -1,1 +1,2 @@
 exports.getWebsiteLangStringName=function() {return document.documentElement.lang || 'en';};
+exports.getFullWebsiteLangStringName=function() {return document.documentElement.lang || 'en-US';};
