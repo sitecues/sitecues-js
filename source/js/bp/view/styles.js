e3e8673b2144066d94620df6d3e2d7229ab41540
Aaron Leventhal <aleventhal@aisquared.com>
2015-04-24 14:50:54 -0400

    Fix style pollution of sitecues toolbar on codecanyon.net. r=tony

--- source/js/bp/view/styles.js
+++ source/js/bp/view/styles.js
@@ -179,6 +179,7 @@ sitecues.def('bp/view/styles', function (styling, callback) {
           'z-index': '9999999',
           'transition': 'opacity 1.5s',
           'transform-origin': '0% 0%',
+          'text-align': 'left', // To prevent style pollution found on http://codecanyon.net/
 //          'will-change': 'transform',   // Removing helps Chrome not get blurry on sitecues.com after zoom
           'outline': 0 // don't show default dotted focus outline
         },
