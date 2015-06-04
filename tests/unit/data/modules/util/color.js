0bbc04ca7f07d817d1e71a8a9c633b7f694a028c
Aaron Leventhal <aleventhal@aisquared.com>
2015-06-04 17:02:45 -0400

    Fix unit tests

---
+++ tests/unit/data/modules/util/color.js
@@ -0,0 +1,41 @@
+exports.isDarkColor = function(colorValue, optionalThreshold) {
+  return false;
+};
+
+exports.isOnDarkBackground = function(current, optionalThreshold) {
+  return false;
+}
+
+exports.getColorString = function(rgba) {
+  return 'black';
+};
+
+exports.getRgbaIfLegalColor = function(color) {
+  return {r: 0, g: 0, b: 0, a: 1 };
+};
+
+exports.getRgba = function(color) {
+  return {r: 0, g: 0, b: 0, a: 1 };
+};
+
+// From http://www.w3.org/TR/2006/WD-WCAG20-20060427/complete.html#luminosity-contrastdef
+exports.getLuminosityFromColorName = function(colorName) {
+  return 0.5;
+};
+
+exports.getLuminosity = function(rgb) {
+  return 0.5;
+};
+
+exports.getContrastRatio = function(color1, color2) {
+  return 5;
+};
+
+exports.rgbToHsl = function(r, g, b) {
+  return { h: 0, s: 0, l: 0 };
+};
+
+// Get the current background color
+exports.getDocumentBackgroundColor = function() {
+  return {r: 255, g: 255, b: 255};
+};
