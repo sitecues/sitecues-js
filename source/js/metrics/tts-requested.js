abb32c7c03e1767c2cd6b1c63814e621d2033491
Seth Holladay <sholladay@aisquared.com>
2015-06-10 16:34:47 -0400

    More styling enhancements.
    More conformance to conventions. Cleaner separation of where internal utilities end and the module API begins. Also naming functions for stack traces and throwing in a comment that I don't expect this module to work in IE. r=anardi

--- source/js/metrics/tts-requested.js
+++ source/js/metrics/tts-requested.js
@@ -12,35 +12,37 @@ sitecues.def('metrics/tts-requested', function (ttsRequested, callback) {
   var TTS_AUDIO_FORMATS = ['ogg', 'mp3', 'aac'];
 
   var DEFAULT_STATE = {
-    'name': 'tts-requested',
-    'trigger': SPEECH_TRIGGERS[0],       // For now, it's always HLB
-    'audio_format': TTS_AUDIO_FORMATS[0],
-    'char_count': 0,
-    'request_time': 0                   // The number of milliseconds the TTS request took to complete.
+    'name'         : 'tts-requested',
+    'trigger'      : SPEECH_TRIGGERS[0],   // For now, it's always HLB.
+    'audio_format' : TTS_AUDIO_FORMATS[0],
+    'char_count'   : 0,
+    'request_time' : 0                     // Number of milliseconds the TTS request took to complete.
   };
 
   sitecues.use('metrics/util', 'jquery', function (metricsUtil) {
 
-    // ============= The API ======================
-    ttsRequested.init = initTTSRequestedData;
-    ttsRequested.update = function (data) {
-        metricsUtil.update(ttsRequested, data);
-    };
-    ttsRequested.send = function () {
-        metricsUtil.send(ttsRequested);
-    };
-    ttsRequested.reset = function () {
-        ttsRequested.update(DEFAULT_STATE);
-    };
+    function init(ttsUrl) {
 
-    function initTTSRequestedData(ttsUrl) {
-      ttsRequested.data = DEFAULT_STATE;
-
+      // TODO: The "URL" constructor does not exist in any version of IE.
+      //       Is this expected to work there?
       var text = getQueryVariable(new URL(ttsUrl), 't');
 
+      ttsRequested.data = DEFAULT_STATE;
       ttsRequested.data.audio_format = getTTSAudioFormat(ttsUrl);
       ttsRequested.data.char_count   = decodeURIComponent(text.replace(/\+/g,  " ")).length;
       ttsRequested.data.request_time = 1;
+    }
+
+    function update(data) {
+      metricsUtil.update(ttsRequested, data);
+    }
+
+    function send() {
+      metricsUtil.send(ttsRequested);
+    }
+
+    function reset() {
+        ttsRequested.update(DEFAULT_STATE);
     }
 
     function getTTSAudioFormat(ttsUrl) {
@@ -53,9 +55,9 @@ sitecues.def('metrics/tts-requested', function (ttsRequested, callback) {
 
       var query = url.search.substring(1),
           vars = query.split("&"),
-          pair;
+          i, pair;
 
-      for (var i=0;i<vars.length;i++) {
+      for (i = 0; i < vars.length; i++) {
         pair = vars[i].split("=");
         if (pair[0] == variable) {
           return pair[1];
@@ -68,21 +70,21 @@ sitecues.def('metrics/tts-requested', function (ttsRequested, callback) {
     // ============= Events Handlers ======================
     // Create an instance on audio speech-play event.
     sitecues.on('audio/speech-play', function (ttsUrl) {
-      if (!ttsRequested['data']) {
+      if (!ttsRequested.data) {
         ttsRequested.init(ttsUrl);
       }
       sitecues.emit('metrics/tts-requested/create');
     });
 
     sitecues.on('metrics/update', function (metrics) {
-      if (ttsRequested['data']) {
+      if (ttsRequested.data) {
         ttsRequested.update(metrics.data);
       }
     });
 
     // Update requested time
     sitecues.on('audio/playing', function (metrics) {
-      if (ttsRequested['data']) {
+      if (ttsRequested.data) {
         ttsRequested.update(metrics.data);
 
         // Send the metric data and clear an instance data on hlb opened(ready) event.
@@ -90,6 +92,12 @@ sitecues.def('metrics/tts-requested', function (ttsRequested, callback) {
         ttsRequested.reset();
       }
     });
+
+    // ============= The API ======================
+    ttsRequested.init = init;
+    ttsRequested.update = update;
+    ttsRequested.send = send;
+    ttsRequested.reset = reset;
 
     // Done.
     callback();
