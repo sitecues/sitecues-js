70d9e7cbce1460d51b627da676e937af4c883a30
Seth Holladay <sholladay@aisquared.com>
2015-06-10 16:04:05 -0400

    Cleanup style to conform to conventions. r=anardi

--- source/js/metrics/tts-requested.js
+++ source/js/metrics/tts-requested.js
@@ -6,7 +6,7 @@
  * This event creation should wait until the user preferences are loaded, and the UI is initialized.
  */
 
-sitecues.def('metrics/tts-requested', function (TTSRequested, callback) {
+sitecues.def('metrics/tts-requested', function (ttsRequested, callback) {
 
   var SPEECH_TRIGGERS = ['space', 'shift', 'shift+m', 'shift+a'];
   var TTS_AUDIO_FORMATS = ['ogg', 'mp3', 'aac'];
@@ -22,65 +22,72 @@ sitecues.def('metrics/tts-requested', function (TTSRequested, callback) {
   sitecues.use('metrics/util', 'jquery', function (metricsUtil) {
 
     // ============= The API ======================
-    TTSRequested.init = initTTSRequestedData;
-    TTSRequested.update = function (data) {
-        metricsUtil.update(TTSRequested, data);
+    ttsRequested.init = initTTSRequestedData;
+    ttsRequested.update = function (data) {
+        metricsUtil.update(ttsRequested, data);
     };
-    TTSRequested.send = function () {
-        metricsUtil.send(TTSRequested);
+    ttsRequested.send = function () {
+        metricsUtil.send(ttsRequested);
     };
-    TTSRequested.reset = function() {
-        TTSRequested.update(DEFAULT_STATE);
-    };
-
-    function initTTSRequestedData(TTSUrl) {
-      TTSRequested.data = DEFAULT_STATE;
-
-      var text = getQueryVariable(new URL(TTSUrl), 't');
-
-      TTSRequested.data.audio_format = getTTSAudioFormat(TTSUrl);
-      TTSRequested.data.char_count = decodeURIComponent(text.replace(/\+/g,  " ")).length;
-      TTSRequested.data.request_time = 1;
+    ttsRequested.reset = function () {
+        ttsRequested.update(DEFAULT_STATE);
     };
 
-    function getTTSAudioFormat(TTSUrl) {
-      var TTSQueryURL = TTSUrl.split('/')[8];
-      var TTSFileName = TTSQueryURL.split('?')[0];
-      return TTSFileName.split('.')[1];
+    function initTTSRequestedData(ttsUrl) {
+      ttsRequested.data = DEFAULT_STATE;
+
+      var text = getQueryVariable(new URL(ttsUrl), 't');
+
+      ttsRequested.data.audio_format = getTTSAudioFormat(ttsUrl);
+      ttsRequested.data.char_count   = decodeURIComponent(text.replace(/\+/g,  " ")).length;
+      ttsRequested.data.request_time = 1;
     }
 
-    function getQueryVariable(url, variable)
-    {
-      var query = url.search.substring(1);
-      var vars = query.split("&");
+    function getTTSAudioFormat(ttsUrl) {
+      var ttsQueryURL = ttsUrl.split('/')[8];
+      var ttsFileName = ttsQueryURL.split('?')[0];
+      return ttsFileName.split('.')[1];
+    }
+
+    function getQueryVariable(url, variable) {
+
+      var query = url.search.substring(1),
+          vars = query.split("&"),
+          pair;
+
       for (var i=0;i<vars.length;i++) {
-        var pair = vars[i].split("=");
-        if(pair[0] == variable){return pair[1];}
+        pair = vars[i].split("=");
+        if (pair[0] == variable) {
+          return pair[1];
+        }
       }
+
       return(false);
     }
 
     // ============= Events Handlers ======================
-    // Create an instance on hlb create event.
-    sitecues.on('audio/speech-play', function(TTSUrl) {
-      if (!TTSRequested['data']) {
-        TTSRequested.init(TTSUrl);
+    // Create an instance on audio speech-play event.
+    sitecues.on('audio/speech-play', function (ttsUrl) {
+      if (!ttsRequested['data']) {
+        ttsRequested.init(ttsUrl);
       }
       sitecues.emit('metrics/tts-requested/create');
     });
 
-    sitecues.on('metrics/update', function(metrics) {
-      TTSRequested['data'] && TTSRequested.update(metrics.data);
+    sitecues.on('metrics/update', function (metrics) {
+      if (ttsRequested['data']) {
+        ttsRequested.update(metrics.data);
+      }
     });
 
     // Update requested time
-    sitecues.on('audio/playing', function(metrics) {
-      if (TTSRequested['data']) {
-        TTSRequested.update(metrics.data);
+    sitecues.on('audio/playing', function (metrics) {
+      if (ttsRequested['data']) {
+        ttsRequested.update(metrics.data);
 
         // Send the metric data and clear an instance data on hlb opened(ready) event.
-        TTSRequested.send();
-        TTSRequested.reset();
+        ttsRequested.send();
+        ttsRequested.reset();
       }
     });
 
