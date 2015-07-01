4dadd7f20c5aa895b6485e6d51aa489d7f619dd4
Aaron Leventhal <aleventhal@aisquared.com>
2015-07-01 13:55:17 -0400

    SC-2949. Refactor focus module. r=seth, heidi

--- source/js/keys/focus.js
+++ source/js/keys/focus.js
@@ -1,118 +1,72 @@
 // focus enhancement (make focus easier to see)
 sitecues.def('focus', function(focus, callback) {
 
-	// minimum zoom at which focus
-	// enhancement appears
-	focus.minzoom = 1.4;
+  'use strict';
 
-	// color of focus enhancement
-	focus.color = 'rgba(82,168,236,.8)';
+  // depends on jquery and conf modules
+  sitecues.use('jquery', 'conf', function($, conf){
 
-	// selector for elements focus
-	// enhancement should be applied
-	//focus.selector = 'input';
-	focus.selector = 'input,a,textarea,[tabindex],button,select';
+    var MIN_ZOOM = 1.4,   // minimum zoom at which focus enhancement appears
+      FOCUS_RING_COLOR = 'rgba(82,168,236,.8)',    // color of focus enhancement
+      $styleSheet = $(),
+      isEnabled;
 
-	// depends on jquery and conf modules
-	sitecues.use('jquery', 'conf', function($, conf){
+    // show focus enhancement
+    function show() {
+      // hide focus first to allow
+      // recalculate outline thickness
+      hide();
 
-		// show focus enhancement
-		focus.show = function(){
-			// private variables
-			var thickness;
+      // create style element
+      // append focus css rule to it
+      // Rounded, soft outline outside of element
+      // TODO -- change z-index and use position: relative if statically positioned so outline on top
+      // 'z-index: 999999; position: relative;' +
+      $styleSheet = $('<style>')
+        .html('*:focus{box-shadow:0 0 3pt 2pt ' + FOCUS_RING_COLOR + ' !important;}')
+        .appendTo('head');
+    }
 
-			// hide fous first to allow
-			// recalculate outline thickness
-			focus.hide();
+    // hide focus enhancement
+    function hide() {
+      $styleSheet.remove();
+    }
 
-			// calculate thickness for current zoom level
-			thickness = 2 + (conf.get('zoom') / 1.7);
+    // refresh focus enhancement bindings on the page
+    function refreshFeatureEnablement (){
+      if (isEnabled) {
+        // if focus enhancement is enabled,
+        // bind `blur` and `focus` events to
+        // proper handlers. use selector for
+        // filtering of matched elements
+        show();
+      } else {
+        // unbind event handlers if focus
+        // enhancement is disabled
+        hide();
+      }
+    }
 
-			// create style element
-			focus.element = $('<style>');
+    // subscribe to zoom changes and update
+    // enhancement state with each change
+    conf.get('zoom', function(currZoom) {
+      // remember previous state of focus
+      var wasEnabled = isEnabled;
 
-			// append focus css rule to it
-			focus.element.html(
-        '*:focus { ' +
-        // Rounded, soft outline outside of element
-        'box-shadow: 0px 0px 3pt 2pt ' + focus.color + ' !important; ' +
-        // TODO -- change z-index and use position: relative if statically positioned so outline on top
-        // 'z-index: 999999; position: relative;' +
-        '}'
-			);
+      // determinate should focus enhancement
+      // be enabled or not
+      isEnabled = currZoom >= MIN_ZOOM;
 
-			// appent style element to head
-			focus.element.appendTo('head');
-		}
+      // if state of enhancement was changed
+      // refresh module bindings on the page
+      if (wasEnabled !== isEnabled) {
+        refreshFeatureEnablement();
+      }
+    });
 
-		// hide focus enhancement
-		focus.hide = function(){
-			// if there is something to remove
-			if (focus.element){
-				// remove element from DOM
-				focus.element.remove();
+    // done
+    callback();
 
-				// set focus element to undefined
-				focus.element = undefined;
-			}
-		}
-
-		// handle element blur
-		focus.blur = function(){
-			// double-check if focus enhancement
-			// is enabled and show focus element
-			if (focus.enabled) focus.hide();
-		}
-
-		// handle
-		focus.focus = function(){
-			// double-check if focus enhancement
-			// is enabled and hide focus element
-			if (focus.enabled) focus.show();
-		}
-
-		// refresh focus enhancement bindings on the page
-		focus.refresh = function(){
-            var $body = $('body');
-			if (focus.enabled){
-				// if focus enhancement is enabled,
-				// bind `blur` and `focus` events to
-				// proper handlers. use selector for
-				// filtering of matched elements
-				$body.
-					on('blur', focus.selector, focus.blur).
-					on('focus', focus.selector, focus.focus);
-
-			} else {
-				// unbind event handlers if focus
-				// enhancement is disabled
-				$body.
-					off('blur', focus.selector, focus.blur).
-					off('focus', focus.selector, focus.focus);
-
-				// hide focus element
-				focus.hide();
-			}
-		}
-
-		// subscribe to zoom changes and update
-		// enhancement state with each change
-		conf.get('zoom', function(zoom){
-			// remember previous state of focus
-			var was = focus.enabled;
-
-			// determinate should focus enhancement
-			// be enabled or not
-			focus.enabled = zoom >= focus.minzoom;
-
-			// if state of enhancement was changed
-			// refresh module bindings on the page
-			if (was !== focus.enabled) focus.refresh();
-		});
-
-		// done
-		callback();
-
-	});
+  });
 
 });
