ce3ae6d33c99fe33f70278c77032fa094c1b4907
Aaron Leventhal <aleventhal@aisquared.com>
2015-08-20 16:07:34 -0400

    Builds and compiles some of the code using r.js

--- core.mk
+++ core.mk
@@ -5,6 +5,35 @@
 #	by this file must be exported in 'makefile'.
 ################################################################################
 
+################################################################################
+# Load the custom configuration file
+################################################################################
+
+# Not a fan of special cases, but 'common' is a special case.
+ifeq ($(custom-config-name), common)
+	custom-name=common
+	custom-files=
+	# The common build does not modify the version, etc...
+	custom-suffix=
+	custom-suffix-upper=
+else
+	# Include the 'configuration' file.
+	include custom-config/$(custom-config-name).mk
+	# The custom builds append their name to the version, etc...
+	custom-suffix=-$(custom-name)
+	custom-suffix-upper=-$(shell echo $(custom-name) | $(to-upper))
+endif
+
+ifeq ($(sc-local), true)
+	# Build a version that doesn't use AJAX for settings, config or metrics -- can be used locally or pasted into a console
+	extra-debug-flags="SC_LOCAL=true,"
+else
+	extra-debug-flags="SC_LOCAL=false,"
+endif
+
+# Make a build-specific version.
+custom-version=$(version)$(custom-suffix-upper)
+
 # Set up the build-specific directory and package name.
 build-basedir:=target
 build-dir:=$(build-basedir)/$(custom-name)
@@ -13,6 +42,7 @@ package-file-name:=$(package-name).tgz
 package-basedir:=$(build-dir)/package
 package-dir:=$(package-basedir)/$(package-name)
 
+
 ################################################################################
 # TARGET: build
 ################################################################################
@@ -20,31 +50,17 @@ build:
 	@echo "===== STARTING: Building '$(custom-name)' library ====="
 	@echo
 
-	@mkdir -p $(build-dir)/source/js
-
-	@sed 's%0.0.0-UNVERSIONED%'$(custom-version)'%g' source/js/core.js > $(build-dir)/source/js/core.js
-
 	@mkdir -p $(build-dir)/compile/js
-
-	@uglifyjs -m -c dead_code=true --define SC_DEV=false,SC_UNIT=false,SC_LOCAL=false -o $(build-dir)/compile/js/sitecues.js $(files)
-
-#Copy files for Source-Maps
-
-	@mkdir -p $(build-dir)/etc/js
-	@(for F in `ls -d source/* | grep -Ev '^source/js$$'` ; do cp -r $$F $(build-dir)/etc ; done)
-	@echo
-
-	@echo "===== GZIP: Creating compressed (gzipped) JavaScript files."
-	@echo
-	@(cd $(build-dir)/compile/js ; for FILE in *.js ; do \
-		gzip -c $$FILE > $$FILE.gz ; \
-	done)
-
+	@node node_modules/.bin/r.js -o baseUrl=source/js generateSourceMaps=true preserveLicenseComments=false optimize=uglify2 name=core out=target/compile/js/sitecues.js
+	# Copy all files to final target directory
+	@cp -R target/compile/js/* $(build-dir)/compile/js
+	# Overwrite sitecues.js with one that has the version set
+	@sed 's%0.0.0-UNVERSIONED%'$(custom-version)'%' target/compile/js/sitecues.js > $(build-dir)/compile/js/sitecues.js
 
 	@echo "* File sizes$(min-bsbel):"
 	@(cd $(build-dir)/compile/js ; \
 	for FILE in `ls *.js *.js.gz | sort` ; do \
-		printf "*  %-16s $$(ls -lh $$FILE | awk '{print($$5);}')\n" $$FILE ; \
+		printf "*  %-16s $$(ls -l $$FILE | awk '{print($$5);}')\n" $$FILE ; \
 	done)
 
 	@echo
@@ -61,19 +77,17 @@ debug:
 	@echo
 
 	@mkdir -p $(build-dir)/compile/js
-	@node node_modules/.bin/r.js -o baseUrl=source/js name=core out=$(build-dir)/compile/js/sitecues.js
-	@uglifyjs $(uglifyjs-args) -o $(build-dir)/compile/js/sitecues.js --source-map $(build-dir)/compile/js/sitecues.js.map --source-map-url sitecues.js.map $(files)
-
-#Copy files for Source-Maps
-	@mkdir -p $(build-dir)/js/source
-	@cp -R source/js $(build-dir)/js/source/
-
+	@node node_modules/.bin/r.js -o baseUrl=source/js name=core generateSourceMaps=true preserveLicenseComments=false optimize=none out=target/compile/js/sitecues.js
+	# Copy all files to final target directory
+	@cp -R target/compile/js/* $(build-dir)/compile/js
+  # Prefix sitecues.js with code to set the global variables. The rest of sitecues.js will be added on top
 	@echo "SC_DEV=true,SC_UNIT=false,"$(extra-debug-flags) > $(build-dir)/compile/js/sitecues.js
-	@(awk 'FNR==1{print ""}1' $(files)) >> $(build-dir)/compile/js/sitecues.js
+	# Add to the final sitecues.js the compiled code with version set
+	@sed 's%0.0.0-UNVERSIONED%'$(custom-version)'%' target/compile/js/sitecues.js >> $(build-dir)/compile/js/sitecues.js
 
-	@mkdir -p $(build-dir)/etc/js
-	@(for F in `ls -d source/* | grep -Ev '^source/js$$'` ; do cp -r $$F $(build-dir)/etc ; done)
-	@echo
+#	@mkdir -p $(build-dir)/etc/js
+#	@(for F in `ls -d source/* | grep -Ev '^source/js$$'` ; do cp -r $$F $(build-dir)/etc ; done)
+#	@echo
 
 	@echo "===== GZIP: Creating compressed (gzipped) JavaScript files."
 	@echo
@@ -84,15 +98,15 @@ debug:
 
 	@echo "* File sizes$(min-label):"
 	@(cd $(build-dir)/compile/js ; \
-	for FILE in `ls *.js *.js.gz | sort` ; do \
-		printf "*  %-16s $$(ls -lh $$FILE | awk '{print($$5);}')\n" $$FILE ; \
+	for FILE in `ls *.js | sort` ; do \
+		printf "*  %-16s $$(ls -l $$FILE | awk '{print($$5);}')\n" $$FILE ; \
 	done)
 
-	@echo
-	@echo "===== COMPLETE: Building '$(custom-name)' library (DEBUG VER) ====="
-	@echo
-	@echo "===== VERSION: $(custom-version)"
-	@echo
+#	@echo
+#	@echo "===== COMPLETE: Building '$(custom-name)' library (DEBUG VER) ====="
+#	@echo
+#	@echo "===== VERSION: $(custom-version)"
+#	@echo
 
 ################################################################################
 # TARGET: package
