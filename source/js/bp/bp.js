1f1c2c8798cfc93a786e7a9370865ca3f5f45134
Aaron Leventhal <aleventhal@aisquared.com>
2015-08-21 13:42:48 -0400

    Build BP

--- source/js/bp/bp.js
+++ source/js/bp/bp.js
@@ -17,14 +17,12 @@
 // bp/will-shrink  -- BP is about to shrink
 // bp/did-shrink   -- BP has finished shrinking
 
-define('bp/model/state','bp/view/modes/badge', 'bp/view/modes/panel', 'bp/helper', 'bp/view/svg', 'bp/constants',
-  'zoom', 'bp/placement', 'bp/view/elements/slider',
-  'bp/size-animation', 'platform', 'conf/site', 'util/color',
-  function (state, badge, panel, helper, bpSVG, BP_CONST, zoomMod,
-            placement, slider, sizeAnimation, platform, site, colorUtil) {
+define('bp/model/state','bp/view/modes/badge', 'bp/view/modes/panel', 'bp/helper', 'bp/view/svg', 'bp/constants', 'zoom/zoom',
+  'bp/placement', 'bp/view/elements/slider', 'bp/size-animation', 'platform', 'conf/site', 'util/color',
+  function (state, badge, panel, helper, bpSVG, BP_CONST, zoomMod, placement, slider, sizeAnimation, platform, site, colorUtil) {
 
   'use strict';
-  // So many dependencies...
+
   /*
    *** Public methods ***
    */
