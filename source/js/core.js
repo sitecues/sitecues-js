0b9b24df0639e6892358fdb51fda1f339d7d8f9b
Aaron Leventhal <aleventhal@aisquared.com>
2015-08-21 17:07:15 -0400

    Update TODOs

--- source/js/core.js
+++ source/js/core.js
@@ -3,21 +3,23 @@
  *   The core module of the sitecues library.
  */
 
-// TODO remove jquery dependency from user-id.js
+// Now
+// TODO remove jquery dependency from user-id.js and others
+// TODO self-initializing modules should not load most code unless necessary, including those that init from settings
+// TODO defer lots of stuff until user prefs ready (initialize settings-listener?)
+//      Or fire conf/did-complete?
+// TODO check on server.js -- doesn't seem like the code to store data on the server actually gets used!
+
+// Later
 // TODO clean up metrics
 // TODO defer page-visited and possibly other metrics until user-id.js finishes getting reply (thus cookie with user id is set)
 //      user-id/did-complete
-// TODO defer lots of stuff until user prefs ready
-//      conf/did-complete
-// TODO check on server.js -- doesn't seem like the code to store data on the server actually gets used!
 // TODO load ie9.js if necessary
 // TODO move other weird ie9 code to ie9.js
 // TODO remove effects/animate from our custom build of jquery to save another 8k/3k
 // TODO load custom scripts
 //      source/js/custom-scripts/custom-scripts.js \
 //      $(custom-files) \
-// TODO Only set things on sitecues object in core.js exportPublicFields()
-// TODO self-initializing modules should not load most code unless necessary, including those that init from settings
 
 define([
   'conf/user/user-id',
