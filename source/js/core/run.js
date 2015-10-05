473c2aa6c9dcf182786367b5dfebb662189c305e
Aaron Leventhal <aleventhal@aisquared.com>
2015-10-05 15:45:05 -0400

    Update TODOs

--- source/js/core/run.js
+++ source/js/core/run.js
@@ -4,11 +4,8 @@
  */
 
 // High priority
-// - use kbd to go back, slider is squished
-// Enter key on
 // / French, Spanish
 // / Accessibility
-// - NVDA just calls bottom link buttons toggle button with no name
 // / SC-3104 -- after an hour of use no TTS?
 // / SC-3120 -- sitecues TTS and Safari
 // - About panel
