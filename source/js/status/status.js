836550ba6b21929cfebfbe840eab736e7bb9b8ad
Seth Holladay <sholladay@aisquared.com>
2015-10-14 16:35:53 -0400

    Fix indentation in status module.

--- source/js/status/status.js
+++ source/js/status/status.js
@@ -1,148 +1,156 @@
-define(['core/conf/urls', 'core/util/xhr', 'core/conf/user/manager', 'core/conf/site'], function (urls, xhr, conf, site) {
+define(
+  [
+    'core/conf/urls',
+    'core/util/xhr',
+    'core/conf/user/manager',
+    'core/conf/site'
+  ],
+  function (urls, xhr, conf, site) {
 
-  function format(object) {
+    function format(object) {
 
-    // Helper to turn objects into string representations for logging.
+      // Helper to turn objects into string representations for logging.
 
-    var INDENTATION = '    ',
-          result = object;
+      var INDENTATION = '    ',
+            result = object;
 
-    if (JSON && JSON.stringify) {
-      result = JSON.stringify(object, null, INDENTATION);
+      if (JSON && JSON.stringify) {
+        result = JSON.stringify(object, null, INDENTATION);
+      }
+
+      return result;
     }
 
-    return result;
-  }
+    function consoleCallback(status) {
 
-  function consoleCallback(status) {
+      // The default status formatter, logs all data to the console.
 
-    // The default status formatter, logs all data to the console.
-
-    // Make sure we are not running from a file (unit testing in node)...
-    if (location.protocol === 'http:' || location.protocol === 'https:') {
-      // We only support the native console for now, so make sure it exists...
-      if (console && console.log) {
-        // Make it clear where to begin copying...
-        console.log('\n-----BEGIN SITECUES STATUS-----\n');
-        // Log with pretty-print, if possible...
-        console.log(format(status));
-        // Make it clear where to end copying...
-        console.log('\n-----END SITECUES STATUS-----\n');
+      // Make sure we are not running from a file (unit testing in node)...
+      if (location.protocol === 'http:' || location.protocol === 'https:') {
+        // We only support the native console for now, so make sure it exists...
+        if (console && console.log) {
+          // Make it clear where to begin copying...
+          console.log('\n-----BEGIN SITECUES STATUS-----\n');
+          // Log with pretty-print, if possible...
+          console.log(format(status));
+          // Make it clear where to end copying...
+          console.log('\n-----END SITECUES STATUS-----\n');
+        }
       }
     }
-  }
 
-  function status(callback) {
+    function status(callback) {
 
-    var html = document.documentElement,
-        confData = conf.data(),
-        coordinates,
-        ajaxUrls = {  // Set the server URLs for retrieving the status of our services (version info, etc.)
-          up : urls.getPrefsUrl('status'),
-          ws : urls.getApiUrl('util/status')
+      var html = document.documentElement,
+          confData = conf.data(),
+          coordinates,
+          ajaxUrls = {  // Set the server URLs for retrieving the status of our services (version info, etc.)
+            up : urls.getPrefsUrl('status'),
+            ws : urls.getApiUrl('util/status')
+          },
+          setting,
+          state,
+          info;
+
+      callback = callback || consoleCallback;
+
+      info = {
+        time       : Date.now(),
+        currentUrl : location.href,
+        userAgent  : navigator.userAgent,
+        version    : {
+          js : sitecues.version,
+          up : null,
+          ws : null
         },
-        setting,
-        state,
-        info;
+        config     : site.getSiteConfig(),
+      };
+      // Measurements useful for reproducing bugs, because their state affects
+      // the behavior of our CSS, animations, etc.
+      coordinates = {
+        document : {
+          clientWidth  : html.clientWidth,
+          clientHeight : html.clientHeight,
+          clientLeft   : html.clientLeft,
+          clientTop    : html.clientTop
+        },
+        window : {
+          pageXOffset  : pageXOffset,
+          pageYOffset  : pageYOffset,
+          innerWidth   : innerWidth,
+          innerHeight  : innerHeight,
+          outerWidth   : outerWidth,
+          outerHeight  : outerHeight,
+          screenX      : screenX,
+          screenY      : screenY
+        },
+        screen : {
+          width        : screen.width,
+          height       : screen.height,
+          availWidth   : screen.availWidth,
+          availHeight  : screen.availHeight,
+          availLeft    : screen.availLeft,
+          availTop     : screen.availTop
+        }
+      };
 
-    callback = callback || consoleCallback;
-
-    info = {
-      time       : Date.now(),
-      currentUrl : location.href,
-      userAgent  : navigator.userAgent,
-      version    : {
-        js : sitecues.version,
-        up : null,
-        ws : null
-      },
-      config     : site.getSiteConfig(),
-    };
-    // Measurements useful for reproducing bugs, because their state affects
-    // the behavior of our CSS, animations, etc.
-    coordinates = {
-      document : {
-        clientWidth  : html.clientWidth,
-        clientHeight : html.clientHeight,
-        clientLeft   : html.clientLeft,
-        clientTop    : html.clientTop
-      },
-      window : {
-        pageXOffset  : pageXOffset,
-        pageYOffset  : pageYOffset,
-        innerWidth   : innerWidth,
-        innerHeight  : innerHeight,
-        outerWidth   : outerWidth,
-        outerHeight  : outerHeight,
-        screenX      : screenX,
-        screenY      : screenY
-      },
-      screen : {
-        width        : screen.width,
-        height       : screen.height,
-        availWidth   : screen.availWidth,
-        availHeight  : screen.availHeight,
-        availLeft    : screen.availLeft,
-        availTop     : screen.availTop
+      // Add current settings (zoom level, etc) to the log.
+      for (setting in confData) {
+        if (confData.hasOwnProperty(setting)) {
+          info[setting] = confData[setting];
+        }
       }
-    };
-
-    // Add current settings (zoom level, etc) to the log.
-    for (setting in confData) {
-      if (confData.hasOwnProperty(setting)) {
-        info[setting] = confData[setting];
+      // Add all measurements for bug reproduction to the log.
+      for (state in coordinates) {
+        if (coordinates.hasOwnProperty(state)) {
+          info[state] = coordinates[state];
+        }
       }
+
+      // Defer the ajax calls so we can respond when both are complete.
+      function readyCheck() {
+        var ready = typeof info.version.up === 'string' &&
+                    typeof info.version.ws === 'string';
+
+        if (ready) {
+          // Publish the status for later retrieval.
+          sitecues.status.latest = info;
+          callback(info);
+        }
+      }
+
+      xhr.getJSON({
+        url:      ajaxUrls.up,
+        success: function (response) {
+          // Set the version based on the AJAX response object
+          info.version.up = response.version;
+          readyCheck();
+        },
+        error: function () {
+          // Set an error message if the AJAX object did not return
+          info.version.up = 'Error fetching UP version from service URL';
+          readyCheck();
+        }
+      });
+
+      xhr.getJSON({
+        type:     'GET',
+        url:      ajaxUrls.ws,
+        success: function (response) {
+          // Set the version based on the AJAX response object
+          info.version.ws = response.version;
+          readyCheck();
+        },
+        error: function () {
+          // Set an error message if the AJAX object did not return
+          info.version.ws = 'Error fetching WS version from service URL';
+          readyCheck();
+        }
+      });
+
+      return 'Fetching sitecues status...';
     }
-    // Add all measurements for bug reproduction to the log.
-    for (state in coordinates) {
-      if (coordinates.hasOwnProperty(state)) {
-        info[state] = coordinates[state];
-      }
-    }
 
-    // Defer the ajax calls so we can respond when both are complete.
-    function readyCheck() {
-      var ready = typeof info.version.up === 'string' &&
-                  typeof info.version.ws === 'string';
-
-      if (ready) {
-        // Publish the status for later retrieval.
-        sitecues.status.latest = info;
-        callback(info);
-      }
-    }
-
-    xhr.getJSON({
-      url:      ajaxUrls.up,
-      success: function (response) {
-        // Set the version based on the AJAX response object
-        info.version.up = response.version;
-        readyCheck();
-      },
-      error: function () {
-        // Set an error message if the AJAX object did not return
-        info.version.up = 'Error fetching UP version from service URL';
-        readyCheck();
-      }
-    });
-
-    xhr.getJSON({
-      type:     'GET',
-      url:      ajaxUrls.ws,
-      success: function (response) {
-        // Set the version based on the AJAX response object
-        info.version.ws = response.version;
-        readyCheck();
-      },
-      error: function () {
-        // Set an error message if the AJAX object did not return
-        info.version.ws = 'Error fetching WS version from service URL';
-        readyCheck();
-      }
-    });
-
-    return 'Fetching sitecues status...';
+    return status;
   }
-
-  return status;
-});
+);
