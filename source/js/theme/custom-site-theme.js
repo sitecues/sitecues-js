64d672ff112883bb19c063c2d7e492055a6aa00b
Aaron Leventhal <aleventhal@aisquared.com>
2015-11-18 16:04:53 -0500

    Dark theme fix for nyise.org

--- source/js/theme/custom-site-theme.js
+++ source/js/theme/custom-site-theme.js
@@ -13,6 +13,7 @@ define(['core/conf/site', 'core/conf/urls'], function(site, urls) {
       's-2158b12a': 1,
       's-25cecd79': 1,
       's-570759e3': 1,
+      's-6d6f89a3': 1,
       's-c27fa71d': 1
     },
     isInitialized;
