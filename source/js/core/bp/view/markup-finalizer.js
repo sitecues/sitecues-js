d0d8778e281d2b1c872683fae9dab1786401149e
Aaron Leventhal <aleventhal@aisquared.com>
2015-12-01 09:44:26 -0500

    SC-3277. Badge not visible and not all elements displayed in panel wokiss.pl subpage.

--- source/js/core/bp/view/markup-finalizer.js
+++ source/js/core/bp/view/markup-finalizer.js
@@ -1,30 +1,21 @@
 // Fix urls and localize strings in markup
 define(['core/locale', 'core/platform'], function(locale, platform) {
-  // The original base URL for the current page regardless of <base> tag
-  function removeEnd(loc) {
-    var locString = '' + loc; // Convert to string
-    return locString.substring(0, locString.lastIndexOf('/'));
-  }
-
   function removeHash(loc) {
     return loc.replace(/\#.*/, '');
   }
 
-  function getBaseURI() {
-    var link = document.createElement('a');
-    link.href = '';
-    return link.href;
-  }
-
-  function hasAlteredBaseURI() {
-    return removeEnd(getBaseURI()) !== removeEnd(document.location.href);
+  // Returns truthy value if there is a base tag
+  function hasBaseTag() {
+    return document.getElementsByTagName('base').length;
   }
 
   // Relative URLs must be full URLS that <base> tag doesn't mess them up!
   // Without this fix, markup such as xlink:href="#foo" or filter="url(#foo)" will not work in Firefox
   // when the source document uses a <base> tag.
+  // Even if the <base> tag points to the default base, we still need to convert hashes, otherwise a page
+  // such as http://wokiss.pl/szkolenia.html will have an invisible badge in some browsers.
   function convertRelativeUrlsToAbsolute(text) {
-    if (hasAlteredBaseURI() && !platform.browser.isIE9) {
+    if (hasBaseTag() && !platform.browser.isIE9) {
       var MATCH_URLS = /(href="|url\()(?:#)/g,
         pageUrlMinusHash = removeHash(document.location.href);
 
