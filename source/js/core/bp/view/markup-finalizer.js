ebe317ae6fc9f6819469fd0d88c850925edc80bc
Aaron Leventhal <aleventhal@aisquared.com>
2015-12-01 11:16:22 -0500

    Always prefix the hashes with the current URL rather than requiring a different base.

--- source/js/core/bp/view/markup-finalizer.js
+++ source/js/core/bp/view/markup-finalizer.js
@@ -4,18 +4,13 @@ define(['core/locale', 'core/platform'], function(locale, platform) {
     return loc.replace(/\#.*/, '');
   }
 
-  // Returns truthy value if there is a base tag
-  function hasBaseTag() {
-    return document.getElementsByTagName('base').length;
-  }
-
   // Relative URLs must be full URLS that <base> tag doesn't mess them up!
   // Without this fix, markup such as xlink:href="#foo" or filter="url(#foo)" will not work in Firefox
   // when the source document uses a <base> tag.
   // Even if the <base> tag points to the default base, we still need to convert hashes, otherwise a page
   // such as http://wokiss.pl/szkolenia.html will have an invisible badge in some browsers.
   function convertRelativeUrlsToAbsolute(text) {
-    if (hasBaseTag() && !platform.browser.isIE9) {
+    if (!platform.browser.isIE9) {
       var MATCH_URLS = /(href="|url\()(?:#)/g,
         pageUrlMinusHash = removeHash(document.location.href);
 
