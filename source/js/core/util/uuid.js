b38008735a1927765d25e79b91ab986d9964fc19
Aaron Leventhal <aleventhal@aisquared.com>
2015-12-21 09:52:11 -0500

    Adjust comment formatting. Explain that params on public function are not used

--- source/js/core/util/uuid.js
+++ source/js/core/util/uuid.js
@@ -2,20 +2,20 @@
 // jshint -W016
 define([], function() {
 
-  function getUUID(a, b) {
+  function getUUID(a, b) {     // Don't use parameters -- this is a micro-optimization to avoid using 'var a,b'
     for (
       b = a = '';              // b - result , a - numeric variable
       a++ < 36;
       b += a * 51&52 ?         // if "a" is not 9 or 14 or 19 or 24
         (                      //  return a random number or 4
           a^15 ?               // if "a" is not 15
-            8 ^ Math.random()* // generate a random number from 0 to 15
-            (a ^ 20 ? 16 : 4)  // unless "a" is 20, in which case a random number from 8 to 11
+            8 ^ Math.random()* //   generate a random number from 0 to 15
+            (a ^ 20 ? 16 : 4)  //   unless "a" is 20, in which case a random number from 8 to 11
           :
-            4                  //  otherwise 4
+            4                  // otherwise 4
         ).toString(16)
       :
-        '-'                    //  in other cases (if "a" is 9,14,19,24) insert "-"
+        '-'                    // in other cases (if "a" is 9,14,19,24) insert "-"
     ) {}
 
     return b;
