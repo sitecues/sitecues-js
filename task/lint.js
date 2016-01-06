20997bbf8123a42e17d45d4df7d58ff10a321a29
Aaron Leventhal <aleventhal@aisquared.com>
2016-01-06 14:35:19 -0500

    gulp js-lint should lint everything

--- task/lint.js
+++ task/lint.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var gulp = require('gulp'),
-  config = require('./build-config'),
   jshint = require('gulp-jshint');
 
 function lint() {
-  var lintFileName = 'source/js/.jshintrc' + (config.isDebugOn ? '-debug' : '');
-  return gulp.src([config.jsGlob, '!source/js/**/jquery.js', '!source/js/**/zepto.js' ])
-  .pipe(jshint(lintFileName))
+  var LINT_GLOB = [
+    'source/js/**/*.js', '!source/js/**/jquery.js', '!source/js/**/zepto.js',
+    'extension/source/js/**/*.js', '!extension/source/js/templated-code/**/*',
+    'gulpfile.js', 'task/**/*.js',
+    'test/**/*.js', '!test/legacy/**/*.js'
+  ];
+  return gulp.src(LINT_GLOB)
+  .pipe(jshint())
   .pipe(jshint.reporter('jshint-stylish'))
   .pipe(jshint.reporter('fail'));
 }
