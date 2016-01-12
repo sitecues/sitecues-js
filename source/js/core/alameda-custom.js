b0aae57c7d5479e420876e8eab229d81e0be03ce
Aaron Leventhal <aleventhal@aisquared.com>
2016-01-12 09:45:26 -0500

    Alameda drives me crazy by making this difficult to debug. It swallows errors (e.g. when I a file doesn't make it into a bundle that is needed for a require()) but sitecues still fails anyway. If we're gonna fail, can I have the debugger give me useful stacks and messages please?

--- source/js/core/alameda-custom.js
+++ source/js/core/alameda-custom.js
@@ -187,15 +187,12 @@ var requirejs, require, define;
                     }
                     called = true;
 
-                    try {
-                        var then = v && v.then;
-                        if (!isFunObj(v) || typeof then !== 'function') {
-                            p[prop] = v;
-                            notify(listeners, v);
-                        }
-                    } catch (e) {
-                        called = false;
-                        f.reject(e);
+                    var then = v && v.then;
+                    // Do not catch exceptions -- it makes debugging easier when they fall through.
+                    // After all, sitecues will fail either way
+                    if (!isFunObj(v) || typeof then !== 'function') {
+                        p[prop] = v;
+                        notify(listeners, v);
                     }
                 }
 
@@ -217,15 +214,15 @@ var requirejs, require, define;
                     var next = prim(function (nextResolve, nextReject) {
 
                         function finish(fn, nextFn, v) {
-                            try {
-                                if (fn && typeof fn === 'function') {
-                                    v = fn(v);
-                                    nextResolve(v);
-                                } else {
-                                    nextFn(v);
-                                }
-                            } catch (e) {
-                                nextReject(e);
+                            // Unlike standard alameda, which tries to swallow errors,
+                            // we want to actually throw on errors so we can find and debug.
+                            // After all, sitecues won't function either way.
+                            // Makes debugging so much easier!!!!
+                            if (fn && typeof fn === 'function') {
+                                v = fn(v);
+                                nextResolve(v);
+                            } else {
+                                nextFn(v);
                             }
                         }
 
@@ -241,11 +238,7 @@ var requirejs, require, define;
                 }
             };
 
-            try {
-                fn(f.resolve, f.reject);
-            } catch (e) {
-                f.reject(e);
-            }
+            fn(f.resolve, f.reject);  // Throw errors on exceptions to make failures easier to debug. sitecues won't work either way
 
             return promise;
         };
@@ -516,6 +509,9 @@ var requirejs, require, define;
                     if (urlFetched[url]) {
                         return;
                     }
+                    if (SC_DEV) {
+                      console.log('New dependency: ' + id);
+                    }
                     urlFetched[url] = true;
 
                     script = document.createElement('script');
@@ -684,6 +680,10 @@ var requirejs, require, define;
             var d = getDefer(name);
 
             d.promise.catch(errback);
+
+            if (SC_DEV && name) {
+              console.log('Loading module: ' + name);
+            }
 
             //Use name if no relName
             relName = relName || name;
