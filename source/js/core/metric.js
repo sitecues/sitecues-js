868b6ee99231c2a280c100e91d6fc80a34d97225
Aaron Leventhal <aleventhal@aisquared.com>
2016-01-19 17:47:33 -0500

    SC-3383. Make session id persist for session in tab. Use a different field name for slider-changed setting name. Add siteId. r=ben
    commit 9e445be910c7c716b141f0727a49c2789bf781fd
    Author: Aaron Leventhal <aleventhal@aisquared.com>
    Date:   Tue Jan 19 12:00:43 2016 -0500
    
        Different field name

--- source/js/core/metric.js
+++ source/js/core/metric.js
@@ -4,8 +4,19 @@
 define(['core/conf/user/manager', 'core/util/uuid', 'core/conf/site', 'core/locale', 'core/platform', 'core/util/xhr', 'core/conf/urls'],
   function (conf, uuid, site, locale, platform, xhr, urls) {
 
-    var sessionId = uuid(),
-      METRICS_VERSION = 2;
+    var pageViewId = uuid(),
+      sessionId = getSessionId(),
+      METRICS_VERSION = 3;
+
+    function getSessionId() {
+      var SESSION_ID_KEY = '-sc-session-id',
+        id = window.sessionStorage.getItem(SESSION_ID_KEY);
+      if (!id) {
+        id = uuid();
+        window.sessionStorage.setItem(SESSION_ID_KEY, id);
+      }
+      return id;
+    }
 
     // IMPORTANT! Increment METRICS_VERSION this every time metrics change in any way
     // IMPORTANT! Have the backend team review all metrics changes!!!
@@ -27,7 +38,8 @@ define(['core/conf/user/manager', 'core/util/uuid', 'core/conf/site', 'core/loca
           clientTimeMs: +new Date(),    // Epoch time in milliseconds  when the event occurred
 
           // Who (details about session and user)
-          sessionId: sessionId,   // A random UUID v4 generated for this library session.
+          sessionId: sessionId,   // A random UUID v4 for this session -- stable between page loads in the same tab
+          pageViewId: pageViewId,  // A random UUID v4 for this page view
           userId: conf.getUserId(),
           zoomLevel: conf.get('zoom') || 1,
           ttsState: conf.get('ttsOn') || false,
@@ -36,6 +48,7 @@ define(['core/conf/user/manager', 'core/util/uuid', 'core/conf/site', 'core/loca
 
           // Where
           pageUrl: location.href,
+          siteId: site.getSiteId(),
 
           // Specifics
           details: details
