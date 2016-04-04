7e5414814e6670340547a924b358b8741d6d0e04
Aaron Leventhal <aleventhal@aisquared.com>
2016-04-04 10:08:26 -0400

    Begin Swedish l10n

---
+++ source/js/locale-data/sv.js
@@ -0,0 +1,48 @@
+/*
+File for English wording.
+ */
+define([], function() {
+  return {
+    // ARIA labels
+    // - Main
+    sitecues_main_panel: 'sitecues huvudpanel',
+    badge_label: 'sitecues zoom- och läsverktyg; ??? sitecues assists users who do not have their own screen reader or magnifier.',
+    zoom_in: 'zooma in',
+    zoom_out: 'zooma ut',
+    // - Secondary
+    more_features: 'More features',   
+    previous: 'Previous',             
+    next: 'Next',                     
+    // - Feedback
+    rating: 'Rating',                 
+    rating_1: '1 star',               
+    rating_2: '2 stars',              
+    rating_3: '3 stars',              
+    rating_4: '4 stars',              
+    rating_5: '5 stars',              
+
+    // Visible labels
+    // - Main
+    pre_zoom: 'Zoom ',
+    post_zoom: 'x',
+    zoom_off: 'Zooma Av',
+    speech: 'Tal',
+    on: 'På',
+    off: 'Av',
+    // - Secondary
+    tips: 'Tips',                     
+    settings: 'Settings',             
+    rate_us: 'Rate us',               
+    rate_us_prompt: 'We would love to hear from you!',  
+    rate_us_suggest: 'Have questions, comments or suggestions?', 
+    about: 'About',                   
+    // - Feedback
+    send: 'Send',                     
+    tell_us_something: 'Tell us something ...',      
+    thank_you: 'Thank you',                          
+    // - About
+    about_preamble: 'This website has added sitecues to make it easier to see, hear, and use. For more information, please visit ',
+    about_rate_us: 'We encourage you to provide feedback on your experience.',
+    zoom_and_speech: 'zoom &amp; speech'             
+  };
+});
