1836acf8e4bb5052c2d9854d2d435c35547d8f71
Aaron Leventhal <aleventhal@aisquared.com>
2016-04-04 11:19:22 -0400

    Extension still needs to run if extension marker was set

--- source/js/core/core.js
+++ source/js/core/core.js
@@ -1,6 +1,6 @@
 define(['core/conf/site', 'core/conf/urls', 'core/run', 'core/constants', 'core/events'], function (site, urls, run, constants, events) {
 
-  if (document.documentElement.hasAttribute('data-sitecues-everywhere')) {
+  if (!SC_EXTENSION && document.documentElement.hasAttribute('data-sitecues-everywhere')) {
     // Don't run if the extension has planted its flag.
     // (The sitecues.exists field is not accessible here if it's set in an extension context.)
     safe_production_msg('Sitecues Everywhere prioritized over Sitecues from webpage.');
