dd4424ba5862b7d8646d3334d260f5be60d4b172
Aaron Leventhal <aleventhal@aisquared.com>
2016-05-05 14:04:07 -0400

    SC-3605. Use new CSS proxy which handles redirects. r=gradetsky

--- source/js/page/style-service/css-aggregator.js
+++ source/js/page/style-service/css-aggregator.js
@@ -68,11 +68,16 @@ define(['$', 'page/style-service/user-agent-css', 'core/conf/site', 'core/conf/u
 
   // CSS proxy passes us the CSS text whether or not cross-origin policy allows it
   // Example of page that needs this: http://www.dcmetrobln.org/about-us
-  // If Brian updates server to use Graham's latest, we may need to
-  // update to new URL format that passes url as a parameter ala css-proxy?url=foo
   function getCssProxyUrl(url) {
-    var absoluteUrl = urls.resolveUrl(url);
-    return urls.getApiUrl('css-proxy/' + absoluteUrl); // TODO encodeURIComponent(absoluteUrl));
+    if (url.substring(0, 5) === 'data:') {
+      return url;
+    }
+
+    var absoluteUrl = urls.resolveUrl(url),
+      apiUrl = urls.getApiUrl('css/passthrough/?url=' + encodeURIComponent(absoluteUrl));
+
+    // TODO remove this line when dev servers are updated
+    return apiUrl.replace('/ws.dev.', '/ws.');
   }
 
   /**
