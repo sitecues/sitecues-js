a18f0f5fd5257153a56fd38797c1a471a8f9c344
Aaron Leventhal <aleventhal@aisquared.com>
2016-05-09 16:00:24 -0400

    Try to fix linting error by linting ES5 and ES6 code separately

--- task/lint.js
+++ task/lint.js
@@ -3,31 +3,34 @@
 var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
   check = require('gulp-check'),
-  amdCheck = require('gulp-amdcheck');
-
-function lint() {
-  var LINT_GLOB = [
-    'source/js/**/*.js', '!source/js/**/jquery.js', '!source/js/core/alameda-custom.js',
+  amdCheck = require('gulp-amdcheck'),
+  ES5_LINT_GLOB = [ 'source/js/**/*.js', '!source/js/**/jquery.js', '!source/js/core/alameda-custom.js' ],
+  ES6_LINT_GLOB = [
     'extension/source/js/**/*.js', '!extension/source/js/templated-code/**/*',
     // TODO lint tests
     //'test/**/*.js', '!test/legacy/**/*.js'.
     'gulpfile.js', 'task/**/*.js'
   ];
 
-  return gulp.src(LINT_GLOB)
+
+function lintES5() {
+  return gulp.src(ES5_LINT_GLOB)
   .pipe(jshint())
   .pipe(jshint.reporter('jshint-stylish'))
   .pipe(jshint.reporter('fail'));
 }
 
+function lintES6() {
+  return gulp.src(ES6_LINT_GLOB)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+}
+
 // Don't allow Function.prototype.bind() usage as it conflicts with Mootools
 // It's okay if it is commented -- preceded by //
 function checkForBindUsage() {
-  var LINT_GLOB = [
-    'source/js/**/*.js'
-  ];
-
-  return gulp.src(LINT_GLOB)
+  return gulp.src(ES5_LINT_GLOB)
     .pipe(check(/\.bind *\(/))
     .on('error', function (err) {
       console.log('Don\'t use Function.prototype.bind() as it is incompatible with Mootools:\n' + err);
@@ -35,11 +38,7 @@ function checkForBindUsage() {
 }
 
 function checkAmd() {
-  var LINT_GLOB = [
-    'source/js/**/*.js'
-  ];
-
-  return gulp.src(LINT_GLOB)
+  return gulp.src(ES5_LINT_GLOB)
     .pipe(amdCheck({
       errorOnUnusedDependencies: true,
       logUnusedDependencyPaths: false
