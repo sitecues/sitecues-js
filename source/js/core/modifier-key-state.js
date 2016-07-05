bd71a97ed141d70a546137cfee28eb427067a299
Aaron Leventhal <aleventhal@aisquared.com>
2016-07-05 17:10:17 -0400

    Remove debugging code

--- source/js/core/modifier-key-state.js
+++ source/js/core/modifier-key-state.js
@@ -9,20 +9,17 @@ define([ 'core/dom-events', 'core/constants' ], function(domEvents, constants) {
   
   function onKeyDown(event) {
     if (event.keyCode === CTRL_KEY_CODE) {
-      console.log('down');
       isCtrlKeyDownNow = true;
     }
   }
 
   function onKeyUp(event) {
     if (event.keyCode === CTRL_KEY_CODE) {
-      console.log('up');
       isCtrlKeyDownNow = false;
     }
   }
 
   function isCtrlKeyDown() {
-    console.log('isdown ' +isCtrlKeyDownNow);
     return isCtrlKeyDownNow;
   }
 
