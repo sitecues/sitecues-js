6a23bb8940370da6bba1cbcd2d25e50085c1395d
Seth Holladay <sholladay@aisquared.com>
2016-08-17 02:04:18 -0400

    Simplify metrics code.

--- source/js/core/metric.js
+++ source/js/core/metric.js
@@ -39,54 +39,15 @@ define(
         platformData,
         sessionData;
 
-
     function Metric(name, details) {
-      this.createDataJSON(name, details);
-      this.sent = false;
-    }
-
-    function logNameCollisions(metricName, data, details) {
-      // In dev, log field name collisions because the backend is going to flatten the metrics object
-      function logFieldNameCollision(propName) {
-        if (data.hasOwnProperty(propName)) {
-          console.error('Sitecues error: name collision for metric ' + metricName + ' field name ' + propName);
-        }
-      }
-
-      if (details) {
-        Object.keys(details).forEach(logFieldNameCollision);
-      }
-    }
-
-    // Data must be of simple types
-    function flattenData(data) {
-      function flattenDataField(propName) {
-        var value = data[propName],
-          type = typeof value;
-        if (value !== null && type !== 'boolean' && type !== 'number' && type !== 'string' && type !== 'undefined') {
-          data[propName] = nativeFn.JSON.stringify(data[propName]);
-        }
-      }
-
-      Object.keys(data).forEach(flattenDataField);
-    }
-
-    // TODO: Delete this and use Object.assign() when we drop IE support.
-    function assign(target, source) {
-      if (source) {
-        Object.keys(source).forEach(function (key) {
-          target[key] = source[key];
-        });
-      }
-    }
-
-    Metric.prototype.createDataJSON = function createDataJSON(name, details) {
       if (doLogMetrics) {
         console.log('Metric / %s', name + (details ? ' / ' + nativeFn.JSON.stringify(details) : ''));
       }
 
-      var data = this.data = {},
-        settings = getSettings(); // Gets all prefs
+      this.sent = false;
+
+      var data = this.data = {};
+      var settings = getSettings(); // Gets all prefs
 
       // Session data
       assign(data, sessionData);
@@ -113,7 +74,41 @@ define(
 
       data.details = details;
       data.settings = settings;
-    };
+    }
+
+    // In dev, log field name collisions because the backend is going to flatten the metrics object
+    function logNameCollisions(metricName, data, details) {
+      function logFieldNameCollision(propName) {
+        if (data.hasOwnProperty(propName)) {
+          console.error('Sitecues error: name collision for metric ' + metricName + ' field name ' + propName);
+        }
+      }
+
+      if (details) {
+        Object.keys(details).forEach(logFieldNameCollision);
+      }
+    }
+
+    // Data must be of simple types.
+    function flattenData(data) {
+      Object.keys(data).forEach(function (propName) {
+        var value = data[propName];
+        var type = typeof value;
+
+        if (value !== null && type !== 'boolean' && type !== 'number' && type !== 'string' && type !== 'undefined') {
+          data[propName] = nativeFn.JSON.stringify(data[propName]);
+        }
+      });
+    }
+
+    // TODO: Delete this and use Object.assign() when we drop IE support.
+    function assign(target, source) {
+      if (source) {
+        Object.keys(source).forEach(function (key) {
+          target[key] = source[key];
+        });
+      }
+    }
 
     Metric.prototype.send = function send() {
       /*jshint validthis: true */
@@ -122,8 +117,8 @@ define(
       }
 
       xhr.post({
-        url: urls.getApiUrl('metrics/site/' + this.data.siteId + '/notify.json?name=' + this.data.name),
-        data: this.data
+        url  : urls.getApiUrl('metrics/site/' + this.data.siteId + '/notify.json?name=' + this.data.name),
+        data : this.data
       });
 
       metricHistory.push(this);
@@ -146,7 +141,6 @@ define(
     }
 
     function isTester() {
-
       if (conf.get('isTester')) {
         // Once a tester, always a tester
         return true;
@@ -162,7 +156,7 @@ define(
 
     // TODO Should go away once we go to the new extension which is entirely in a content script
     function isOldExtension() {
-      return sitecues.everywhereConfig;
+      return Boolean(sitecues.everywhereConfig);
     }
 
     // Return settings we care about
@@ -218,7 +212,6 @@ define(
     }
 
     function init() {
-
       if (isInitialized) {
         return;
       }
