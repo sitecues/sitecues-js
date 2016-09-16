83c60adfc0dafad748de5655f546c2c787a477ae
Aaron Leventhal <aleventhal@aisquared.com>
2016-09-16 04:45:05 -0400

    Address review comments

--- source/js/core/ab-test/config.js
+++ source/js/core/ab-test/config.js
@@ -37,7 +37,7 @@ define([], function () {
       // Test Marc's idea to have an extra sensitive badge for first time users
       weight: 1
     },
-    'moreButtonTimerGood': {
+    'moreButtonTimerV2': {
       weight: 1,
       values: [
         0,
