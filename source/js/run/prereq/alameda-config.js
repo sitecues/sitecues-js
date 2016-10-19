f39c7589af975fade3f8157f3bdf773a192d555c
Aaron Leventhal <aleventhal@aisquared.com>
2016-10-19 16:13:44 -0400

    Ready for review

--- source/js/run/prereq/alameda-config.js
+++ source/js/run/prereq/alameda-config.js
@@ -1,17 +1,15 @@
 // Alameda configuration
 // Must be inserted before alameda by the build process
 
-/* globals SC_VERSION  */
-
 sitecues._require = {
   defaultErrback: function(error) {
     var event = new CustomEvent('SitecuesRequireFailure', {detail: error});
     window.dispatchEvent(event);
   },
   waitSeconds: 30,
-  baseUrl: (function(config) {
-    return config.appUrl.split('/').slice(0, -3).join('/') + '/' + sitecues.version + '/js/';
-  })(sitecues.everywhereConfig || sitecues.config),
+  baseUrl: (function(config, version) {
+    return config.appUrl.split('/').slice(0, -3).join('/') + '/' + version + '/js/';
+  })(sitecues.config, sitecues.version),
   map: {
     '*': {
       '$': 'page/jquery/jquery'
