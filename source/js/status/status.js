fe0ba0c95d8f1465132aa16c05a66e63dbd42fcc
Aaron Leventhal <aleventhal@aisquared.com>
2016-12-01 09:19:00 -0500

    Fix exception in feedvback for extension. urls.getApiUrls is not allowed as API calls are not allowed

--- source/js/status/status.js
+++ source/js/status/status.js
@@ -53,7 +53,7 @@ define(
           prefData = pref.get(),
           coordinates,
           ajaxUrls = {  // Set the server URLs for retrieving the status of our services (version info, etc.)
-            ws : urls.getApiUrl('util/status')
+            ws : SC_EXTENSION ? urls.getApiUrl('util/status') : 'not allowed'
           },
           setting,
           state,
